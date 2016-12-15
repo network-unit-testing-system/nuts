2af7cf0cb2cb832cae09ffd5af88df6a8ab60b11
asta1992 <astalder@hsr.ch>
2016-12-15 23:40:42 +0100

    Update FileValidator.py

--- src/service/FileValidator.py
+++ src/service/FileValidator.py
@@ -11,19 +11,24 @@ class FileValidator:
         self.logger = logging.getLogger('nuts_error_log')
 
     def validate(self):
+        cur_dir = os.path.dirname(__file__)
+        testfile = os.path.join(cur_dir, "testSchema.yaml")
+        devfile = os.path.join(cur_dir, "deviceSchema.yaml")
         try:
             if self.deviceFile == "T":
-                procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml'],stdout=subprocess.PIPE, shell=True)
+                procTest = subprocess.Popen(['/opt/nuts/env/bin/pykwalify -d ' +  self.testFile + ' -s ' + testfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                 print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
             elif self.deviceFile == "D":
-                procDev = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml' ], stdout=subprocess.PIPE, shell=True)
+                procDev = subprocess.Popen(['/opt/nuts/env/bin/pykwalify -d ' +  self.deviceFile + ' -s ' + devfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                 print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
             else:
-                procTest = subprocess.Popen(['pykwalify -d ' + self.testFile + ' -s ' + os.getcwd() + '/src/service/testSchema.yaml'],stdout=subprocess.PIPE, shell=True)
-                procDev = subprocess.Popen(['pykwalify -d ' + self.deviceFile + ' -s ' + os.getcwd() + '/src/service/deviceSchema.yaml'],stdout=subprocess.PIPE, shell=True)
+                procTest = subprocess.Popen(['/opt/nuts/env/bin/pykwalify -d ' +  self.testFile + ' -s ' + testfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                procDev = subprocess.Popen(['/opt/nuts/env/bin/pykwalify -d ' +  self.deviceFile + ' -s ' + devfile], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
                 print("Test-File: \n" + procTest.communicate()[0].decode('utf-8'))
                 print("Device-File: \n" + procDev.communicate()[0].decode('utf-8'))
         except Exception as e:
             print("Validator-Fehler")
             self.logger.exception("Validator-Fehler")
+
+
 

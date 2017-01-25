1873674a28a402e0f621b1272f243823f52417b5
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-01-25 11:01:26 +0100

    Change salt_api_wrapper to meet our criterias
    Customize the api_wrapper and document the functions

--- nuts/src/service/salt_api_wrapper.py
+++ nuts/src/service/salt_api_wrapper.py
@@ -7,34 +7,24 @@ class SaltApi(object):
         self.api = Pepper(self.settings.getVariable('NUTS_SALT_REST_API_URL'))
 
     def connect(self):
+        '''connects to the salt-api with the defined username and password.
+        configuration can be done in config.yml in your cwd'''
         self.api.login(self.settings.getVariable('NUTS_SALT_REST_API_USERNAME'),
                        self.settings.getVariable('NUTS_SALT_REST_API_PASSWORD'), 'pam')
 
-    def discovery(self, args):
-        response = self.api.low([{'client': 'local', 'tgt': '*', 'fun': 'state.sls',
-                                  'arg': ['staging.discovery', 'concurrent=True',
-                                          'pillar={"IP_Address":"%s"}' % args.ip]}])
-        return response
 
     def start_task(self, args):
-        response = self.api.low([{'client': 'local_async', 'tgt': '*', 'fun': 'state.sls',
-                                  'arg': ['staging', 'concurrent=True', 'pillar={"SN":"%s"}' % args.sn]}])
+        ''' starts a the function defined in args.function on targets (args.targets) with the parameters defined in args.arguments
+        
+            response format: {u'return': [{u'jid': u'20160718141822884336', u'minions': [u'srv01']}]}
+        '''
+        response = self.api.low([{'client': 'local_async', 'tgt': args.targets, 'fun': args.function,
+                                  'arg': args.arguments}])
 
-        # response format: {u'return': [{u'jid': u'20160718141822884336', u'minions': [u'srv01']}]}
         return response
-
-    def event(self, args):
-        # creates event: salt/netapi/hook/osr/stagingtask/FCZ0000012Z9/rebooted
-        response = self.api.low({'sn': args.sn, 'name': args.name},
-                                path='/hook/osr/stagingtask/%s/%s' % (args.sn, args.name))
-        return response
-
-    def show_low_sls(self):
-        response = self.api.low([{'client': 'local', 'tgt': '*', 'fun': 'state.show_low_sls',
-                                  'arg': [settings.OSR_SALT_STAGINGTASK_STATENAME, 'concurrent=True']}])
-        return response
-
+    
     def abort_task(self, args):
+        '''aborts the task with the taskid defined in args.job_id'''
         response = self.api.low([{'client': 'local_async', 'tgt': '*', 'fun': 'saltutil.kill_job',
                                   'arg': [args.job_id]}])
         return response

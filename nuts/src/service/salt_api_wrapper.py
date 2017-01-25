bb5d6a267478aeff249ce2a08b19c0864f62f6e1
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-01-25 12:39:04 +0100

    Add synchron possibiltiy to start a task

--- nuts/src/service/salt_api_wrapper.py
+++ nuts/src/service/salt_api_wrapper.py
@@ -11,14 +11,23 @@ class SaltApi(object):
         configuration can be done in config.yml in your cwd'''
         self.api.login(self.settings.getVariable('NUTS_SALT_REST_API_USERNAME'),
                        self.settings.getVariable('NUTS_SALT_REST_API_PASSWORD'), 'pam')
-
-
     def start_task(self, args):
         ''' starts a the function defined in args.function on targets (args.targets) with the parameters defined in args.arguments
         
-            response format: {u'return': [{u'jid': u'20160718141822884336', u'minions': [u'srv01']}]}
+            response format: {u'return': [{u'srv01':True,u'srv020:True}]}
         '''
         
+        response = self.api.low([{'client': 'local', 'tgt': args['targets'], 'fun': args['function'],
+                                  'arg': args['arguments']}])
+
+        return response
+
+    def start_task_async(self, args):
+        ''' starts a the function defined in args.function on targets (args.targets) with the parameters defined in args.arguments
+        this function is asnyc and you'll have to collect the results with get_task_result
+        
+            response format: {u'return': [{u'jid': u'20160718141822884336', u'minions': [u'srv01']}]}
+        '''
         response = self.api.low([{'client': 'local_async', 'tgt': args['targets'], 'fun': args['function'],
                                   'arg': args['arguments']}])
 
@@ -29,14 +38,16 @@ class SaltApi(object):
         response = self.api.low([{'client': 'local_async', 'tgt': '*', 'fun': 'saltutil.kill_job',
                                   'arg': [args.job_id]}])
         return response
-    def get_job_result(self,jobresponse=null, jobid=null):
-        '''returns the current result of the entered job.
+    def get_task_result(self,taskresponse=None, taskid=None):
+        '''returns the current result of the entered task.
         you can either insert the response you got when you started the task OR
-        you can insert the jobid
+        you can insert the taskid directly
+        
+        response format: {u'return': [{u'srv01':True,u'srv020:True}]}
         '''
-        if jobid != null and jobresponse != null and  jobresponse['return'][0]['jid'] != jobid:
+        if taskid != None and taskresponse != None and  taskresponse['return'][0]['jid'] != taskid:
             #throw exception
-            raise ValueError('You entered both jobresponse and jobid but they didn\'t match')
-        if jobid == null:
-            jobid = jobresponse['return'][0]['jid']
-        return self.api.lookup_jid(jobid)
+            raise ValueError('You entered both taskresponse and taskid but they didn\'t match')
+        if taskid == None:
+            taskid = jobresponse['return'][0]['jid']
+        return self.api.lookup_jid(taskid)

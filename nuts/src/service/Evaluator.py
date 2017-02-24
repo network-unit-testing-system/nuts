06710e230fa009fd56e6395f88d9d7b94c3b574d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-02-24 15:26:00 +0100

    Change output of evaluator
    Change the way how we format the output

--- nuts/src/service/Evaluator.py
+++ nuts/src/service/Evaluator.py
@@ -9,6 +9,7 @@ class Evaluator:
 
     def compare(self, testCase):
         if testCase.operator == "=":
+            print(testCase.actualResult["result"], testCase.expectedResult)
             return self.comp(testCase.actualResult["result"], testCase.expectedResult)
         elif testCase.operator == "<":
             return testCase.expectedResult < testCase.actualResult["result"]
@@ -18,20 +19,10 @@ class Evaluator:
             return testCase.expectedResult != testCase.actualResult["result"]
 
     def getResult(self, testCase):
-        res = ''
-        if testCase.actualResult["resulttype"] == "single":
-            res = testCase.actualResult["result"]
-        elif testCase.actualResult["resulttype"] == "multiple":
-            res = ' '.join(testCase.actualResult["result"])
-        return res
-
+        return testCase.actualResult['result']
+    
     def getExpected(self, testCase):
-        exp = ''
-        if testCase.actualResult["resulttype"] == "single":
-            exp = testCase.expectedResult
-        elif testCase.actualResult["resulttype"] == "multiple":
-            exp = ' '.join(testCase.expectedResult)
-        return exp
+        return testCase.expectedResult
 
     def comp(self, list1, list2):
         if isinstance(list1,list) and isinstance(list1,list):
@@ -39,6 +30,16 @@ class Evaluator:
         else:
             return list1 == list2
 
+    @staticmethod
+    def formatResult(result):
+        if isinstance(result,basestring):
+            return result.encode('utf-8')
+        if isinstance(result, list):
+            return [x.encode('utf-8') for x in result]
+        if isinstance(result,dict):
+            return {x.encode('utf-8'):y.encode('utf-8') for x,y in result.items()}
+        return str(result)
+    
     def printResult(self, testCase):
         if self.getResult(testCase) == "ERROR":
             print(
@@ -47,7 +48,7 @@ class Evaluator:
                 testCase.name + ": Test nicht bestanden -------------------\nFehler bei der Ausfuehrung des Tests!")
         elif self.compare(testCase):
             print('\033[92m' + testCase.name + ": Test bestanden ------------------------- \033[0m\nExpected: " + str(
-                self.getExpected(testCase)) + " " + testCase.operator + " Actual: " + str(self.getResult(testCase)))
+                self.formatResult(self.getExpected(testCase))) + " " + testCase.operator + " Actual: " + self.formatResult(str(self.getResult(testCase))))
             self.infologger.warning('\n' + testCase.name + ": Test bestanden ------------------------- \nExpected: " + str(
                 self.getExpected(testCase)) + " " + testCase.operator + " Actual:  " + str(self.getResult(testCase)))
         else:

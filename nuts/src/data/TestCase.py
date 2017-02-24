735f6d732ec9a19db0e0123473e4c292d36d226c
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-02-24 17:56:39 +0100

    Added parts of async processing
    Added first parts to process the tests aysnc -> first create all jobs and collect the results after

--- nuts/src/data/TestCase.py
+++ nuts/src/data/TestCase.py
@@ -6,13 +6,19 @@ class TestCase:
         self.parameter = parameter
         self.operator = operator
         self.expectedResult = expectedResult
-
+        self.job_id = ''
+        self.minions = []
+        
     def setActualResult(self, actualResult):
         self.actualResult = actualResult
 
     def getActualResult(self):
         return self.actualResult
-
+    
+    def set_job(self, job_description):
+        self.job_id = job_description['return'][0]['jid']
+        self.minions = job_description['return'][0]['minions']
+        
     def __str__(self):
         return "Name: {0}, Command: {1}, Devices: {2}, Parameter: {3}, Operator: {4}, Expected: {5}".format(self.name,
                                                                                                             self.command,

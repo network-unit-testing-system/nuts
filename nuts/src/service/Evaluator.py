18048e283699e2072a9e313145f2bcde8c632c8d
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-03-09 16:00:54 +0100

    Change manual color coding to color coding of colorama

--- nuts/src/service/Evaluator.py
+++ nuts/src/service/Evaluator.py
@@ -1,5 +1,6 @@
 import datetime
 import logging
+from colorama import Fore
 
 
 class Evaluator:
@@ -40,30 +41,34 @@ class Evaluator:
         return str(result)
 
     def _test_case_failed(self, test_case):
-        return self.get_result(test_case) == "ERROR"
-    
+        return self.get_result(test_case) == 'ERROR'
+
     def validate_result(self, test_case):
         if self._test_case_failed(test_case):
             print(
-                '\033[91m' + test_case.name + ": Test error -------------------\033[0m\An error occurred while executing the test!")
+                Fore.RED + test_case.name + ': Test error -------------------\n' +
+                Fore.RESET + 'An error occurred while executing the test!')
             self.info_logger.warning(
-                test_case.name + ": Test failed -------------------\033[0m\nFailure while executing the test!")
+                test_case.name + ': Test failed -------------------\nFailure while executing the test!')
             self.test_suite.mark_test_case_failed(test_case)
         elif self.compare(test_case):
-            print('\033[92m' + test_case.name + ": Test passed ------------------------- \033[0m\nExpected: " + str(
-                self.format_result(self.get_expected(test_case))) + " " + test_case.operator + " Actual: " + self.format_result(str(self.get_result(test_case))))
-            self.info_logger.warning('\n' + test_case.name + ": Test passed ------------------------- \nExpected: " + str(
-                self.get_expected(test_case)) + " " + test_case.operator + " Actual:  " + str(self.get_result(test_case)))
+            print(Fore.GREEN + test_case.name + ': Test passed -------------------------\n' +
+                  Fore.RESET + 'Expected: ' + str(self.format_result(self.get_expected(test_case))) +
+                  ' ' + test_case.operator + ' Actual: ' + self.format_result(str(self.get_result(test_case))))
+            self.info_logger.warning('\n' + test_case.name + ': Test passed ------------------------- \nExpected: ' +
+                                     str(self.get_expected(test_case)) + ' ' + test_case.operator +
+                                     ' Actual:  ' + str(self.get_result(test_case)))
             self.test_suite.mark_test_case_passed(test_case)
         else:
-            print('\033[91m' + test_case.name + ": Test failed -------------------\033[0m\nExpected: " + str(
-                self.get_expected(test_case)) + " " + test_case.operator + " Actual: " + str(self.get_result(test_case)))
-            self.info_logger.warning('\n' + test_case.name + ": Test failed ------------------- \nExpected: " + str(
-                self.get_expected(test_case)) + " " + test_case.operator + " Actual:  " + str(self.get_result(test_case)))
+            print(Fore.RED + test_case.name + ': Test failed -------------------\n' +
+                  Fore.RESET + 'Expected: ' + str(self.get_expected(test_case)) + ' ' + test_case.operator +
+                  ' Actual: ' + str(self.get_result(test_case)))
+            self.info_logger.warning('\n' + test_case.name + ': Test failed ------------------- \nExpected: ' +
+                                     str(self.get_expected(test_case)) + ' ' + test_case.operator +
+                                     ' Actual:  ' + str(self.get_result(test_case)))
             self.test_suite.mark_test_case_failed(test_case)
 
     def validate_all_results(self):
         for test in self.test_suite.test_cases:
             self.validate_result(test)
         self.test_suite.print_statistics()
-        

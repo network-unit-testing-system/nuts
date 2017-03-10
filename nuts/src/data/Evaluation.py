2b7a1e9bcd9ca5ee6d0bd53727b7bbdbcf74e469
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-03-10 12:35:48 +0100

    Add new classes Evaluation and EvaluationResult for better tracking of multiple results
    The new classes will help to better differenciate between the results of different minions to the same test

---
+++ nuts/src/data/Evaluation.py
@@ -0,0 +1,13 @@
+class Evaluation:
+
+    def __init__(self, expected_result, operator):
+        self.evaluation_results = []
+        self.expected_result = expected_result
+        self.operator = operator
+
+    def result(self):
+        passed = True
+        for result in self.evaluation_results:
+            if not result.passed:
+                passed = False
+        return passed
---
+++ nuts/src/data/EvaluationResult.py
@@ -0,0 +1,6 @@
+class EvaluationResult:
+
+    def __init__(self, minion, actual_result, passed):
+        self.minion = minion
+        self.actual_result = actual_result
+        self.passed = passed

c5f75b6200efef4903d7a05150cad34bf74e4a2e
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2017-03-16 13:36:00 +0100

    Change test_evaluator to use fixtures

--- nuts/test/test_Evaluator.py
+++ nuts/test/test_Evaluator.py
@@ -3,83 +3,87 @@ from src.service.Evaluator import Evaluator
 from src.data.TestSuite import TestSuite
 
 
-class TestEvaluator:
+@pytest.fixture
+def test_suite():
     test_suite = TestSuite("TestTestSuite")
-    eval = Evaluator(test_suite)
+    test_suite.create_test("testPingFromAToB", "connectivity", 'Server01', '8.8.8.8', "=", 'True')
+    test_suite.create_test("checkuser", "checkuser", 'Server02', '', "=", ['admin', 'user'])
+    test_suite.create_test("Count ospf neighbors", "countospfneighbors", 'Switch1', '', "=", '3')
+    test_suite.create_test("checkuser multiple devices", "checkuser", 'cisco.*', '', "=", ['admin', 'user'])
+    return test_suite
 
-    @classmethod
-    def setup_class(cls):
-        cls.test_suite.create_test("testPingFromAToB", "connectivity", 'Server01', '8.8.8.8', "=", 'True')
-        cls.test_suite.create_test("checkuser", "checkuser", 'Server02', '', "=", ['admin', 'user'])
-        cls.test_suite.create_test("Count ospf neighbors", "countospfneighbors", 'Switch1', '', "=", '3')
-        cls.test_suite.create_test("checkuser multiple devices", "checkuser", 'cisco.*', '', "=", ['admin', 'user'])
 
-        cls.eval = Evaluator(cls.test_suite)
+@pytest.fixture
+def evaluator(test_suite):
+    return Evaluator(test_suite)
 
-    def test_comp_multiple_devices(self):
-        test_case = self.test_suite.get_test_by_name('checkuser multiple devices')
+
+class TestEvaluator:
+
+    def test_comp_multiple_devices(self, test_suite, evaluator):
+        test_case = test_suite.get_test_by_name('checkuser multiple devices')
         result = {
             u'cisco.csr.1000v': {"resulttype": "single", "result": ['admin', 'user']},
             u'cisco.test': {"resulttype": "single", "result": ['admin', 'user']}
         }
         test_case.minions = [u'cisco.csr.1000v', u'cisco.test']
         test_case.actual_result = result
-        assert self.eval.compare(test_case).result() is True
+        assert evaluator.compare(test_case).result() is True
 
-    def test_comp_multiple_devices_false(self):
-        test_case = self.test_suite.get_test_by_name('checkuser multiple devices')
+    def test_comp_multiple_devices_false(self, test_suite, evaluator):
+        test_case = test_suite.get_test_by_name('checkuser multiple devices')
         result = {
             u'cisco.csr.1000v': {"resulttype": "single", "result": ['admin', 'user']},
             u'cisco.test': {"resulttype": "single", "result": ['admin']}
         }
         test_case.minions = [u'cisco.csr.1000v', u'cisco.test']
         test_case.actual_result = result
-        assert self.eval.compare(test_case).result() is False
+        assert evaluator.compare(test_case).result() is False
 
-    def test_comp_multiple(self):
+    def test_comp_multiple(self, evaluator):
         list1 = []
         list2 = []
         list1.append('admin')
         list1.append('user')
         list2.append('user')
         list2.append('admin')
-        assert self.eval.comp(list1, list2) is True
+        assert evaluator.comp(list1, list2) is True
 
-    def test_comp_multiple_false(self):
+    def test_comp_multiple_false(self, evaluator):
         list1 = []
         list2 = []
         list1.append('admin')
         list1.append('user')
         list2.append('user')
-        assert self.eval.comp(list1, list2) is False
+        assert evaluator.comp(list1, list2) is False
 
-    def test_compare_single_true(self):
-        test_case = self.test_suite.get_test_by_name('Count ospf neighbors')
+    def test_compare_single_true(self, test_suite, evaluator):
+        test_case = test_suite.get_test_by_name('Count ospf neighbors')
         result = {u'cisco.csr.1000v': {"resulttype": "single", "result": '3'}}
         test_case.minions.append('cisco.csr.1000v')
         test_case.actual_result = result
-        evaluation = self.eval.compare(test_case)
+        evaluation = evaluator.compare(test_case)
         assert evaluation.expected_result == '3'
         assert len(evaluation.evaluation_results) == 1
         assert evaluation.result() is True
 
-    def test_compare_single_false(self):
+    def test_compare_single_false(self, test_suite, evaluator):
         result = {u'cisco.csr.1000v': {"resulttype": "single", "result": 'False'}}
-        test_case = self.test_suite.get_test_by_name('testPingFromAToB')
+        test_case = test_suite.get_test_by_name('testPingFromAToB')
         test_case.minions.append('cisco.csr.1000v')
         test_case.actual_result = result
-        assert self.eval.compare(test_case).result() is False
+        assert evaluator.compare(test_case).result() is False
 
-    def test_compare_multiple_true(self):
+    def test_compare_multiple_true(self, test_suite, evaluator):
         result = {u'cisco.csr.1000v': {"resulttype": "multiple", "result": ['admin', 'user']}}
-        test_case = self.test_suite.get_test_by_name('checkuser')
+        test_case = test_suite.get_test_by_name('checkuser')
         test_case.minions.append('cisco.csr.1000v')
         test_case.actual_result = result
-        assert self.eval.compare(test_case).result() is True
+        assert evaluator.compare(test_case).result() is True
 
-    def test_compare_multiple_false(self):
+    def test_compare_multiple_false(self, test_suite, evaluator):
         result = {u'cisco.csr.1000v': {"resulttype": "multiple", "result": ['admin']}}
-        test_case = self.test_suite.get_test_by_name('checkuser')
+        test_case = test_suite.get_test_by_name('checkuser')
         test_case.minions.append('cisco.csr.1000v')
         test_case.actual_result = result
-        assert self.eval.compare(test_case).result() is False
+        assert evaluator.compare(test_case).result() is False

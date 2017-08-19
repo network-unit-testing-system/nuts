b4998f1b1879669b845543355454fe12b2bcaa62
ubaumann <github@m.ubaumann.ch>
2017-08-19 14:27:00 +0200

    Fix Python3 issus, use past basestring

--- nuts/data/evaluation_result.py
+++ nuts/data/evaluation_result.py
@@ -1,3 +1,7 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+
 class EvaluationResult(object):
     def __init__(self, minion, actual_result, passed):
         self.minion = minion
--- nuts/service/salt_api_wrapper.py
+++ nuts/service/salt_api_wrapper.py
@@ -1,4 +1,8 @@
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
 from pepper import Pepper
+
 from nuts.service.settings import settings
 
 

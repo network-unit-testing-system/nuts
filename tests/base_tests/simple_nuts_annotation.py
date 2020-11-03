db29a7a63df9afef8efc5dbf16c083d44385915e
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-03 07:44:19 +0100

    add further annotation tests

--- tests/base_tests/simple_nuts_annotation.py
+++ tests/base_tests/simple_nuts_annotation.py
@@ -6,3 +6,12 @@ class TestKeyValue:
     @pytest.mark.nuts("key,value", "placeholder")
     def test_key_value(self, key, value):
         assert key == value
+
+
+class TestKeyValueWithoutParameter:
+
+    @pytest.mark.nuts("key,value")
+    def test_key_value(self, key, value):
+        assert key == value
+
+
--- tests/test_nuts_annotation.py
+++ tests/test_nuts_annotation.py
@@ -30,6 +30,22 @@ def test_skips_execution_without_arguments(testdir):
     result.assert_outcomes(skipped=1)
 
 
+def test_execute_tests_errors_with_incomplete_data(testdir):
+    arguments = {
+        'test_class_loading':
+            """
+---
+- test_module: tests.base_tests.simple_nuts_annotation
+  test_class: TestKeyValue
+  arguments: [{"key": "abc"}]
+            """
+    }
+    testdir.makefile('.yaml', **arguments)
+
+    result = testdir.runpytest()
+    result.assert_outcomes(errors=1)
+
+
 def test_execute_tests_multiple_times_separates_arguments(testdir):
     arguments = {
         'test_class_loading':
@@ -47,3 +63,19 @@ def test_execute_tests_multiple_times_separates_arguments(testdir):
 
     result = testdir.runpytest()
     result.assert_outcomes(passed=1, failed=1)
+
+
+def test_execute_tests_errors_without_placeholder(testdir):
+    arguments = {
+        'test_class_loading':
+            """
+---
+- test_module: tests.base_tests.simple_nuts_annotation
+  test_class: TestKeyValueWithoutParameter
+  arguments: [{"key": "abc", "value":"abc"}]
+            """
+    }
+    testdir.makefile('.yaml', **arguments)
+
+    result = testdir.runpytest()
+    result.assert_outcomes(errors=1)

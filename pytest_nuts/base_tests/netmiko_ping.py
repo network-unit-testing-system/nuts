a288aabaee11e2b7889df43152608075a8ae65e5
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-11-04 10:35:11 +0100

    refactor netmiko ping

--- pytest_nuts/base_tests/netmiko_ping.py
+++ pytest_nuts/base_tests/netmiko_ping.py
@@ -20,22 +20,17 @@ class TestNetmikoPing:
                 "delay_factor": delay_factor}
 
     @pytest.fixture(scope="class")
-    def hosts(self, test_execution_params):
-        return {entry["source"] for entry in test_execution_params}
+    def hosts(self, nuts_parameters):
+        return {entry["source"] for entry in nuts_parameters['test_data']}
 
     @pytest.fixture(scope="class")
     def transformed_result(self, general_result):
         raw_textfsm_pinganswer = {key: [v.result for v in value[1:]] for key, value in general_result.items()}
         return {key: parse_ping_results(value) for key, value in raw_textfsm_pinganswer.items()}
 
-    @pytest.fixture(scope="class")
-    def test_execution_params(self, nuts_parameters):
-        return nuts_parameters['test_data']
-
     @pytest.mark.nuts("source,destination,expected", "placeholder")
     def test_ping(self, transformed_result, source, destination, expected):
         assert transformed_result[source][destination].name == expected
-
 
 
 class Ping(Enum):

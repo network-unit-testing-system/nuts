e1a14d538d015354008847c2d87b8b85bc7b9898
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-11-04 09:15:19 +0100

    cleanup code, remove superfluous comments

--- pytest_nuts/yaml2test.py
+++ pytest_nuts/yaml2test.py
@@ -14,7 +14,7 @@ class NutsYamlFile(pytest.File):
         super().__init__(fspath, parent)
 
     def collect(self):
-        # We need a yaml parser, e.g. PyYAML.
+        # TODO We need a yaml parser, e.g. PyYAML.
         import yaml
 
         raw = yaml.safe_load(self.fspath.open())
@@ -22,7 +22,6 @@ class NutsYamlFile(pytest.File):
         for test_entry in raw:
             module = load_module(test_entry.get("test_module"), test_entry.get("test_class"))
             yield NutsTestFile.from_parent(self, fspath=self.fspath, obj=module, test_entry=test_entry)
-            # yield YamlItem.from_parent(self, test_class=test_entry["test_class"],  arguments=test_entry["arguments"])
 
 
 def load_module(module_path, class_name):
@@ -38,11 +37,11 @@ class NutsTestFile(pytest.Module):
         self.test_entry = test_entry
 
     def collect(self) -> Iterable[Union["Item", "Collector"]]:
-        """
-        Collects a single NutsTestClass instance from this NutsTestFile.
+        """Collects a single NutsTestClass instance from this NutsTestFile.
         At the start inject setup_module fixture and parse all fixtures from the module.
         This is directly adopted from pytest.Module
         """
+
         self._inject_setup_module_fixture()
         self._inject_setup_function_fixture()
         self.session._fixturemanager.parsefactories(self)
@@ -51,9 +50,8 @@ class NutsTestFile(pytest.Module):
         label = self.test_entry.get("label")
         name = class_name if label is None else f'{class_name} - {label}'
 
-        test_data = self.test_entry.get('data')
-        test_execution = self.test_entry.get("test_execution")  # optional
-        # TODO: default behaviour OR behaviour that can be overwritten if needed
+        test_data = self.test_entry.get('test_data')
+        test_execution = self.test_entry.get("test_execution")
         yield NutsTestClass.from_parent(self,
                                         name=name,
                                         class_name=class_name,
@@ -70,7 +68,9 @@ class NutsTestClass(pytest.Class):
     def _getobj(self):
         """Get the underlying Python object.
         Overwritten from PyobjMixin to separate name and classname
-        This allows to group multiple tests of the same class with different parameters to be grouped separately"""
+        This allows to group multiple tests of the same class with different parameters to be grouped separately
+        """
+
         # TODO: Improve the type of `parent` such that assert/ignore aren't needed.
         assert self.parent is not None
         obj = self.parent.obj  # type: ignore[attr-defined]
@@ -84,9 +84,12 @@ class NutsTestClass(pytest.Class):
     def collect(self) -> Iterable[Union[nodes.Item, nodes.Collector]]:
         """Inject custom nuts fixture into the test classes
         Similar to the injection of setup_class and setup_method in pytest.Class::collect
+
+        nuts_parameters: Used as fixture for actual tests. Can include optional info on how to run the test.
+        get_parametrizing_data: Used for parametrizing and thus generate tests.
         """
 
-        @fixtures.fixture(scope="class")  # exposed as fixture for Tests
+        @fixtures.fixture(scope="class")
         def nuts_parameters(cls):
             return self.params
 

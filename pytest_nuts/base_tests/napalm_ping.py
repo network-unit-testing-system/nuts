c5016e82c959a9f4afd92e714f0681e161e67611
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-05 13:41:55 +0100

    Fix napalm ping

--- pytest_nuts/base_tests/napalm_ping.py
+++ pytest_nuts/base_tests/napalm_ping.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
 import pytest
-
 from nornir.core import Task
 from nornir.core.task import MultiResult
 from nornir_napalm.plugins.tasks import napalm_ping
@@ -15,9 +14,7 @@ class TestNapalmPing:
     @pytest.fixture(scope="class")
     def nuts_arguments(self, nuts_parameters):
         test_data = nuts_parameters['test_data']
-        delay_factor = nuts_parameters['test_execution']['delay_factor']
-        return {"destinations_per_host": destinations_per_host(test_data),
-                "delay_factor": delay_factor}
+        return {"destinations_per_host": destinations_per_host(test_data)}
 
     @pytest.fixture(scope="class")
     def hosts(self, nuts_parameters):
@@ -25,7 +22,7 @@ class TestNapalmPing:
 
     @pytest.fixture(scope="class")
     def transformed_result(self, general_result):
-        return map_result_to_enum(general_result)
+        return {host: parse_ping_results(task_results) for host, task_results in general_result.items()}
 
     @pytest.mark.nuts("source,destination,expected", "placeholder")
     def test_ping(self, transformed_result, source, destination, expected):
@@ -38,17 +35,13 @@ class Ping(Enum):
     FLAPPING = 2
 
 
-def napalm_ping_multi_host(task: Task, destinations_per_host, delay_factor) -> MultiResult:
+def napalm_ping_multi_host(task: Task, destinations_per_host) -> MultiResult:
     destinations = destinations_per_host(task.host.name)
     results = MultiResult("pinged_hosts")
     for destination in destinations:
-        results.append(
-            task.run(
-                task=napalm_ping,
-                dest=destination,
-                delay_factor=delay_factor,
-            )
-        )
+        result = task.run(task=napalm_ping, dest=destination)
+        result[0].destination = destination
+        results += result
     return results
 
 
@@ -56,11 +49,14 @@ def destinations_per_host(test_data):
     return lambda host_name: [entry["destination"] for entry in test_data if entry["source"] == host_name]
 
 
-def map_result_to_enum(general_result: dict):
-    nornir_answer_by_host = {key: [v.result for v in value[1:]] for key, value in general_result.items()}
-    # TODO nornir/napalm returns an empty list of results when the host does not exist on the network, instead of the required IP
-    # destination: trans[<dict: hosts>][<list:dests>]['success']['results'][<list of result of 5 pings>: 0]['ip_address']
-    # result of ping that must be mapped to enum: trans["R1"][0]['success']['packet_loss']
-    return None
+def parse_ping_results(task_results):
+    return {ping_task.destination: parse_result(ping_task.result['success']) for ping_task in task_results[1:]}
 
 
+
+def parse_result(result):
+    if result['packet_loss'] == 0:
+        return Ping.SUCCESS
+    if result['packet_loss'] == result['probes_sent']:
+        return Ping.FAIL
+    return Ping.FLAPPING

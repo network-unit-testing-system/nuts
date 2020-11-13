32f407e255e4949219533144ca12c2b6688e1c45
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-11-13 12:07:13 +0100

    rename variables to something more informative

--- tests/shared.py
+++ tests/shared.py
@@ -7,8 +7,8 @@ YAML_EXTENSION = ".yaml"
 
 
 def create_multi_result(result_content):
-    multi_result_r1 = MultiResult("napalm_get")
-    result_r1 = Result(host=None, name="naplam_get")
-    result_r1.result = result_content
-    multi_result_r1.append(result_r1)
-    return multi_result_r1
+    multi_result = MultiResult("napalm_get")
+    single_result = Result(host=None, name="napalm_get")
+    single_result.result = result_content
+    multi_result.append(single_result)
+    return multi_result

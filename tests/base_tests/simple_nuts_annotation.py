7343af83a88d6fef8db319bad5ecf966f9e4d14c
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-20 09:15:59 +0100

    strip field names in nuts marker

--- tests/base_tests/simple_nuts_annotation.py
+++ tests/base_tests/simple_nuts_annotation.py
@@ -7,6 +7,12 @@ class TestKeyValue:
         assert key == value
 
 
+class TestSpacedKeyValue:
+    @pytest.mark.nuts(" key , value")
+    def test_key_value(self, key, value):
+        assert key == value
+
+
 class TestOptionalAttribute:
     @pytest.mark.nuts("key,value", "key")
     def test_key_value(self, key, value):

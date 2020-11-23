58831f4d92a699eaf4b15a38330a0c80c26fde8c
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-23 14:41:31 +0100

    update existing tests to use fixture for failed napalm task

--- tests/base_tests/test_napalm_bgp_neighbors.py
+++ tests/base_tests/test_napalm_bgp_neighbors.py
@@ -1,5 +1,4 @@
 import pytest
-from napalm.base.exceptions import ConnectionException
 from nornir.core.task import AggregatedResult
 
 from pytest_nuts.base_tests.napalm_bgp_neighbors import transform_result
@@ -18,7 +17,7 @@ neighbor_details = {
 
 
 @pytest.fixture
-def general_result():
+def general_result(timeouted_multiresult):
     result = AggregatedResult("napalm_get")
     result["R1"] = create_multi_result(
         {
@@ -68,66 +67,7 @@ def general_result():
         }
     )
 
-    result["R3"] = create_multi_result(
-        r"""Traceback (most recent call last):
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 920, in establish_connection
-    self.remote_conn_pre.connect(**ssh_connect_params)
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\client.py", line 349, in connect
-    retry_on_signal(lambda: sock.connect(addr))
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\util.py", line 283, in retry_on_signal
-    return function()
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\client.py", line 349, in <lambda>
-    retry_on_signal(lambda: sock.connect(addr))
-socket.timeout: timed out
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\base\base.py", line 92, in _netmiko_open
-    **netmiko_optional_args
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\ssh_dispatcher.py", line 312, in ConnectHandler
-    return ConnectionClass(*args, **kwargs)
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\cisco\cisco_ios.py", line 17, in __init__
-    return super().__init__(*args, **kwargs)
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 346, in __init__
-    self._open()
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 351, in _open
-    self.establish_connection()
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 942, in establish_connection
-    raise NetmikoTimeoutException(msg)
-netmiko.ssh_exception.NetmikoTimeoutException: TCP connection to device failed.
-
-Common causes of this problem are:
-1. Incorrect hostname or IP address.
-2. Wrong TCP port.
-3. Intermediate firewall blocking access.
-
-Device settings: cisco_ios 10.20.0.123:22
-
-
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\task.py", line 98, in start
-    r = self.task(self, **self.params)
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir_napalm\plugins\tasks\napalm_get.py", line 32, in napalm_get
-    device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\inventory.py", line 448, in get_connection
-    extras=conn.extras,
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\inventory.py", line 499, in open_connection
-    configuration=configuration,
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir_napalm\plugins\connections\__init__.py", line 57, in open
-    connection.open()
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\ios\ios.py", line 170, in open
-    device_type, netmiko_optional_args=self.netmiko_optional_args
-  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\base\base.py", line 95, in _netmiko_open
-    raise ConnectionException("Cannot connect to {}".format(self.hostname))
-napalm.base.exceptions.ConnectionException: Cannot connect to 10.20.0.123
-""",
-        failed=True,
-        exception=ConnectionException("Cannot connect to 10.20.0.123"),
-    )
+    result["R3"] = timeouted_multiresult
     return result
 
 

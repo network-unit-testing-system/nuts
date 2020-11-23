6f80390c2277aa5bd9ed02f74d57c2cf2bec12d7
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-23 14:41:18 +0100

    add transform for users

--- pytest_nuts/base_tests/napalm_get_users.py
+++ pytest_nuts/base_tests/napalm_get_users.py
@@ -1,8 +1,12 @@
 from collections import defaultdict
+from typing import Dict
 
 import pytest
 from nornir.core.filter import F
+from nornir.core.task import MultiResult
 from nornir_napalm.plugins.tasks import napalm_get
+
+from pytest_nuts.helpers.result import nuts_result_wrapper, NutsResult, check_result
 
 
 @pytest.fixture(scope="class")
@@ -29,30 +33,35 @@ def hosts(nuts_parameters):
 def transformed_result(general_result):
     return transform_result(general_result)
 
+@pytest.fixture
+def single_result(transformed_result, host):
+    assert host in transformed_result, f"Host {host} not found in aggregated result."
+    return transformed_result[host]
 
+@pytest.mark.usefixtures("check_result")
 class TestNapalmUsers:
     @pytest.mark.nuts("host,username")
-    def test_username(self, transformed_result, host, username):
-        assert username in transformed_result[host]
+    def test_username(self, single_result, username):
+        assert username in single_result.result
 
     @pytest.mark.nuts("host,username,password")
-    def test_password(self, transformed_result, host, username, password):
-        assert transformed_result[host][username]["password"] == password
+    def test_password(self, single_result, username, password):
+        assert single_result.result[username]["password"] == password
 
     @pytest.mark.nuts("host,username,level")
-    def test_privilege_level(self, transformed_result, host, username, level):
-        assert transformed_result[host][username]["level"] == level
+    def test_privilege_level(self, single_result, username, level):
+        assert single_result.result[username]["level"] == level
 
 
 class TestNapalmOnlyDefinedUsersExist:
     @pytest.mark.nuts("host,usernames")
-    def test_no_rogue_users(self, transformed_result, host, usernames):
-        assert list(transformed_result[host].keys()) == usernames
+    def test_no_rogue_users(self, single_result, usernames):
+        assert list(single_result.result.keys()) == usernames
 
 
-def transform_result(general_result):
-    return {host: _transform_single_result(result) for host, result in general_result.items()}
+def transform_result(general_result) -> Dict[str, NutsResult]:
+    return {host: nuts_result_wrapper(result, _transform_single_result) for host, result in general_result.items()}
 
 
-def _transform_single_result(single_result):
+def _transform_single_result(single_result: MultiResult) -> dict:
     return single_result[0].result["users"]

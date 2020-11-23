a53a6064eaea505508f8389ed2348b0780ad4154
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-23 14:23:59 +0100

    transform bgp neighbors

--- pytest_nuts/base_tests/napalm_bgp_neighbors.py
+++ pytest_nuts/base_tests/napalm_bgp_neighbors.py
@@ -1,6 +1,10 @@
+from typing import Dict
+
 import pytest
 from nornir.core.filter import F
+from nornir.core.task import MultiResult
 from nornir_napalm.plugins.tasks import napalm_get
+from pytest_nuts.helpers.result import nuts_result_wrapper, check_result, NutsResult
 
 
 @pytest.fixture(scope="class")
@@ -28,50 +32,59 @@ def transformed_result(general_result):
     return transform_result(general_result)
 
 
+@pytest.mark.usefixtures("check_result")
 class TestNapalmBgpNeighborsCount:
+    @pytest.fixture
+    def single_result(self, transformed_result, source):
+        assert source in transformed_result, f"Host {source} not found in aggregated result."
+        return transformed_result[source]
+
     @pytest.mark.nuts("source,neighbor_count")
-    def test_neighbor_count(self, transformed_result, source, neighbor_count):
-        assert source in transformed_result
-        assert len(transformed_result[source]) == neighbor_count
+    def test_neighbor_count(self, single_result, neighbor_count):
+        assert len(single_result.result) == neighbor_count
 
 
+@pytest.mark.usefixtures("check_result")
 class TestNapalmBgpNeighbors:
+    @pytest.fixture
+    def single_result(self, transformed_result, source):
+        assert source in transformed_result, f"Host {source} not found in aggregated result."
+        return transformed_result[source]
+
+    @pytest.fixture
+    def peer_result(self, single_result, peer):
+        return single_result.result[peer]
+
     @pytest.mark.nuts("source,peer,local_as")
-    def test_local_as(self, transformed_result, source, peer, local_as):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["local_as"] == local_as
+    def test_local_as(self, peer_result, local_as):
+        assert peer_result["local_as"] == local_as
 
     @pytest.mark.nuts("source,peer,local_id")
-    def test_local_id(self, transformed_result, source, peer, local_id):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["local_id"] == local_id
+    def test_local_id(self, peer_result, local_id):
+        assert peer_result["local_id"] == local_id
 
     @pytest.mark.nuts("source,peer,remote_as")
-    def test_remote_as(self, transformed_result, source, peer, remote_as):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["remote_as"] == remote_as
+    def test_remote_as(self, peer_result, remote_as):
+        assert peer_result["remote_as"] == remote_as
 
     @pytest.mark.nuts("source,peer,remote_id")
-    def test_remote_id(self, transformed_result, source, peer, remote_id):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["remote_id"] == remote_id
+    def test_remote_id(self, peer_result, remote_id):
+        assert peer_result["remote_id"] == remote_id
 
     @pytest.mark.nuts("source,peer,is_enabled")
-    def test_is_enabled(self, transformed_result, source, peer, is_enabled):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["is_enabled"] == is_enabled
+    def test_is_enabled(self, peer_result, is_enabled):
+        assert peer_result["is_enabled"] == is_enabled
 
     @pytest.mark.nuts("source,peer,is_up")
-    def test_is_up(self, transformed_result, source, peer, is_up):
-        bgp_neighbor_entry = transformed_result[source][peer]
-        assert bgp_neighbor_entry["is_up"] == is_up
+    def test_is_up(self, peer_result, is_up):
+        assert peer_result["is_up"] == is_up
 
 
-def transform_result(general_result):
-    return {source: _transform_single_result(result) for source, result in general_result.items()}
+def transform_result(general_result) -> Dict[str, NutsResult]:
+    return {source: nuts_result_wrapper(result, _transform_single_result) for source, result in general_result.items()}
 
 
-def _transform_single_result(single_result):
+def _transform_single_result(single_result: MultiResult) -> dict:
     task_result = single_result[0].result
     neighbors = task_result["bgp_neighbors"]
     if "global" not in neighbors:
@@ -81,6 +94,6 @@ def _transform_single_result(single_result):
     return {peer: _add_local_id(details, router_id) for peer, details in global_scope["peers"].items()}
 
 
-def _add_local_id(element, router_id):
+def _add_local_id(element: dict, router_id: str) -> dict:
     element["local_id"] = router_id
     return element
--- tests/base_tests/test_napalm_bgp_neighbors.py
+++ tests/base_tests/test_napalm_bgp_neighbors.py
@@ -1,4 +1,5 @@
 import pytest
+from napalm.base.exceptions import ConnectionException
 from nornir.core.task import AggregatedResult
 
 from pytest_nuts.base_tests.napalm_bgp_neighbors import transform_result
@@ -66,11 +67,72 @@ def general_result():
             }
         }
     )
+
+    result["R3"] = create_multi_result(
+        r"""Traceback (most recent call last):
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 920, in establish_connection
+    self.remote_conn_pre.connect(**ssh_connect_params)
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\client.py", line 349, in connect
+    retry_on_signal(lambda: sock.connect(addr))
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\util.py", line 283, in retry_on_signal
+    return function()
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\paramiko\client.py", line 349, in <lambda>
+    retry_on_signal(lambda: sock.connect(addr))
+socket.timeout: timed out
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\base\base.py", line 92, in _netmiko_open
+    **netmiko_optional_args
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\ssh_dispatcher.py", line 312, in ConnectHandler
+    return ConnectionClass(*args, **kwargs)
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\cisco\cisco_ios.py", line 17, in __init__
+    return super().__init__(*args, **kwargs)
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 346, in __init__
+    self._open()
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 351, in _open
+    self.establish_connection()
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\netmiko\base_connection.py", line 942, in establish_connection
+    raise NetmikoTimeoutException(msg)
+netmiko.ssh_exception.NetmikoTimeoutException: TCP connection to device failed.
+
+Common causes of this problem are:
+1. Incorrect hostname or IP address.
+2. Wrong TCP port.
+3. Intermediate firewall blocking access.
+
+Device settings: cisco_ios 10.20.0.123:22
+
+
+
+During handling of the above exception, another exception occurred:
+
+Traceback (most recent call last):
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\task.py", line 98, in start
+    r = self.task(self, **self.params)
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir_napalm\plugins\tasks\napalm_get.py", line 32, in napalm_get
+    device = task.host.get_connection(CONNECTION_NAME, task.nornir.config)
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\inventory.py", line 448, in get_connection
+    extras=conn.extras,
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir\core\inventory.py", line 499, in open_connection
+    configuration=configuration,
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\nornir_napalm\plugins\connections\__init__.py", line 57, in open
+    connection.open()
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\ios\ios.py", line 170, in open
+    device_type, netmiko_optional_args=self.netmiko_optional_args
+  File "C:\Users\maede\Documents\nornir-pytest-playground\venv\lib\site-packages\napalm\base\base.py", line 95, in _netmiko_open
+    raise ConnectionException("Cannot connect to {}".format(self.hostname))
+napalm.base.exceptions.ConnectionException: Cannot connect to 10.20.0.123
+""",
+        failed=True,
+        exception=ConnectionException("Cannot connect to 10.20.0.123"),
+    )
     return result
 
 
 class TestTransformResult:
-    @pytest.mark.parametrize("host", ["R1", "R2"])
+    @pytest.mark.parametrize("host", ["R1", "R2", "R3"])
     def test_contains_hosts_at_toplevel(self, general_result, host):
         transformed_result = transform_result(general_result)
         assert host in transformed_result
@@ -78,16 +140,21 @@ class TestTransformResult:
     @pytest.mark.parametrize("host,neighbors", [("R1", ["172.16.255.2", "172.16.255.3"]), ("R2", ["172.16.255.1"])])
     def test_contains_peers_at_second_level(self, general_result, host, neighbors):
         transformed_result = transform_result(general_result)
-        assert list(transformed_result[host].keys()) == neighbors
+        assert list(transformed_result[host].result.keys()) == neighbors
 
     @pytest.mark.parametrize("host,neighbor,details", [("R1", "172.16.255.2", neighbor_details)])
     def test_contains_information_about_neighbor(self, general_result, host, neighbor, details):
         transformed_result = transform_result(general_result)
-        expected_details = transformed_result[host][neighbor]
+        expected_details = transformed_result[host].result[neighbor]
         for key in details:
             assert expected_details[key] == details[key]
 
     @pytest.mark.parametrize("host,neighbor,local_id", [("R1", "172.16.255.2", "172.16.255.1")])
     def test_contains_router_id_as_local_id(self, general_result, host, neighbor, local_id):
         transformed_result = transform_result(general_result)
-        assert transformed_result[host][neighbor]["local_id"] == local_id
+        assert transformed_result[host].result[neighbor]["local_id"] == local_id
+
+    def test_marks_as_failed_if_task_failed(self, general_result):
+        transformed_result = transform_result(general_result)
+        assert transformed_result["R3"].failed
+        assert transformed_result["R3"].exception is not None

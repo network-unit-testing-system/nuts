0be5112373cb014f90405d7445fa7b625d0e1dc6
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-11-24 08:07:49 +0100

    wrapp cdp neighbors

--- pytest_nuts/base_tests/netmiko_cdp_neighbors.py
+++ pytest_nuts/base_tests/netmiko_cdp_neighbors.py
@@ -1,9 +1,15 @@
+from typing import Dict
+
 import pytest
 
 from nornir.core.filter import F
+from nornir.core.task import MultiResult
 from nornir_netmiko import netmiko_send_command
 
+from pytest_nuts.helpers.result import NutsResult, nuts_result_wrapper, check_result
 
+
+@pytest.mark.usefixtures("check_result")
 class TestNetmikoCdpNeighbors:
     @pytest.fixture(scope="class")
     def nuts_task(self):
@@ -23,31 +29,37 @@ class TestNetmikoCdpNeighbors:
 
     @pytest.fixture(scope="class")
     def transformed_result(self, general_result):
-        return {source: result[0].result for source, result in general_result.items()}
+        return transform_result(general_result)
 
-    @pytest.fixture(scope="class")
-    def grouped_result(self, transformed_result):
-        return {
-            source: {neighbor["destination_host"]: neighbor for neighbor in neighbors}
-            for source, neighbors in transformed_result.items()
-        }
+    @pytest.fixture
+    def single_result(self, transformed_result, source):
+        assert source in transformed_result, f"Host {source} not found in aggregated result."
+        return transformed_result[source]
 
     @pytest.fixture
     def destination_list(self, nuts_parameters):
         return nuts_parameters
 
     @pytest.mark.nuts("source,destination_host")
-    def test_destination_host(self, grouped_result, source, destination_host):
-        assert destination_host in grouped_result[source]
+    def test_destination_host(self, single_result, destination_host):
+        assert destination_host in single_result.result
 
     @pytest.mark.nuts("source,destination_host,local_port")
-    def test_local_port(self, grouped_result, source, destination_host, local_port):
-        assert grouped_result[source][destination_host]["local_port"] == local_port
+    def test_local_port(self, single_result, destination_host, local_port):
+        assert single_result.result[destination_host]["local_port"] == local_port
 
     @pytest.mark.nuts("source,destination_host,remote_port")
-    def test_remote_port(self, grouped_result, source, destination_host, remote_port):
-        assert grouped_result[source][destination_host]["remote_port"] == remote_port
+    def test_remote_port(self, single_result, destination_host, remote_port):
+        assert single_result.result[destination_host]["remote_port"] == remote_port
 
     @pytest.mark.nuts("source,destination_host,management_ip")
-    def test_management_ip(self, grouped_result, source, destination_host, management_ip):
-        assert grouped_result[source][destination_host]["management_ip"] == management_ip
+    def test_management_ip(self, single_result, destination_host, management_ip):
+        assert single_result.result[destination_host]["management_ip"] == management_ip
+
+
+def transform_result(general_result) -> Dict[str, NutsResult]:
+    return {source: nuts_result_wrapper(result, _transform_single_result) for source, result in general_result.items()}
+
+
+def _transform_single_result(single_result: MultiResult) -> dict:
+    return {neighbor["destination_host"]: neighbor for neighbor in single_result[0].result}

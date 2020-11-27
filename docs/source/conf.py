e20352cd13af54306c2b3d0ad3b46163b8774cd7
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-11-27 12:14:25 +0100

    add sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
 
-project = 'NUTS - NetTowel Network Unit Testing System'
+project = 'NUTS'
 copyright = '2020, Matthias Gabriel, Méline Sieber, Urs Baumann'
 author = 'Matthias Gabriel, Méline Sieber, Urs Baumann'
 
@@ -30,7 +30,7 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx.ext.autodoc']
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -53,3 +53,7 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'description': 'NetTowel Network Unit Testing System'
+}

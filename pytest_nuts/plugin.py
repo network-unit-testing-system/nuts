551cd378f95843bb3a076fcf04fce79bb2797d5a
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-12-11 10:43:22 +0100

    black corrections

--- pytest_nuts/plugin.py
+++ pytest_nuts/plugin.py
@@ -97,7 +97,9 @@ def wrap_if_needed(source, required_fields, present_fields):
     missing_fields = required_fields - set(source)
     if not missing_fields:
         return present_fields
-    return pytest.param(*present_fields, marks=pytest.mark.skip(f"required values {missing_fields} not present in test-bundle"))
+    return pytest.param(
+        *present_fields, marks=pytest.mark.skip(f"required values {missing_fields} not present in test-bundle")
+    )
 
 
 def dict_to_tuple(source, fields):

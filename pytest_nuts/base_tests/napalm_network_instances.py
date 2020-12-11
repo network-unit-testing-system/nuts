fc7a6768a9a91dd33e8c4e861b85dbd0156d7dd3
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-12-11 15:21:50 +0100

    transform network instances test into context format

--- pytest_nuts/base_tests/napalm_network_instances.py
+++ pytest_nuts/base_tests/napalm_network_instances.py
@@ -4,39 +4,48 @@ import pytest
 from nornir.core.filter import F
 from nornir_napalm.plugins.tasks import napalm_get
 
-from pytest_nuts.helpers.converters import InterfaceNameConverter
 from pytest_nuts.helpers.result import nuts_result_wrapper, NutsResult
+from pytest_nuts.plugin import NutsContext
 
 
-@pytest.fixture(scope="class")
-def nuts_task():
-    return napalm_get
+class NetworkInstancesContext(NutsContext):
+    def nuts_task(self):
+        return napalm_get
+
+    def nuts_arguments(self):
+        return {"getters": ["network_instances"]}
+
+    def nornir_filter(self):
+        hosts = {entry["host"] for entry in self.nuts_parameters["test_data"]}
+        return F(name__any=hosts)
+
+    def transform_result(self, general_result) -> Dict[str, NutsResult]:
+        return {
+            host: nuts_result_wrapper(result, self._transform_single_result) for host, result in general_result.items()
+        }
+
+    def _transform_single_result(self, single_result):
+        task_result = single_result[0].result
+        network_instances = task_result["network_instances"]
+        return {
+            instance: self._transform_single_network_instance(details)
+            for instance, details in network_instances.items()
+        }
+
+    def _transform_single_network_instance(self, network_instance):
+        return {
+            "route_distinguisher": self._extract_route_distinguisher(network_instance),
+            "interfaces": self._extract_interfaces(network_instance),
+        }
+
+    def _extract_route_distinguisher(self, element):
+        return element["state"]["route_distinguisher"]
+
+    def _extract_interfaces(self, element):
+        return list(element["interfaces"]["interface"].keys())
 
 
-@pytest.fixture(scope="class")
-def nuts_arguments():
-    return {"getters": ["network_instances"]}
-
-
-@pytest.fixture(scope="class")
-def nornir_filter(hosts):
-    return F(name__any=hosts)
-
-
-@pytest.fixture(scope="class")
-def hosts(nuts_parameters):
-    return {entry["host"] for entry in nuts_parameters["test_data"]}
-
-
-@pytest.fixture(scope="class")
-def transformed_result(general_result):
-    return transform_result(general_result)
-
-
-@pytest.fixture
-def single_result(transformed_result, host):
-    assert host in transformed_result, f"Host {host} not found in aggregated result."
-    return transformed_result[host]
+CONTEXT = NetworkInstancesContext
 
 
 @pytest.mark.usefixtures("check_nuts_result")
@@ -48,28 +57,3 @@ class TestNapalmNetworkInstances:
     @pytest.mark.nuts("host,network_instance,route_distinguisher")
     def test_route_distinguisher(self, single_result, network_instance, route_distinguisher):
         assert single_result.result[network_instance]["route_distinguisher"] == route_distinguisher
-
-
-def transform_result(general_result) -> Dict[str, NutsResult]:
-    return {host: nuts_result_wrapper(result, _transform_single_result) for host, result in general_result.items()}
-
-
-def _transform_single_result(single_result):
-    task_result = single_result[0].result
-    network_instances = task_result["network_instances"]
-    return {instance: _transform_single_network_instance(details) for instance, details in network_instances.items()}
-
-
-def _transform_single_network_instance(network_instance):
-    return {
-        "route_distinguisher": _extract_route_distinguisher(network_instance),
-        "interfaces": _extract_interfaces(network_instance),
-    }
-
-
-def _extract_route_distinguisher(element):
-    return element["state"]["route_distinguisher"]
-
-
-def _extract_interfaces(element):
-    return list(element["interfaces"]["interface"].keys())

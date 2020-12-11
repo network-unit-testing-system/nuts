cc50ceed3d81386877bc83dd6ce9797988479f96
Matthias Gabriel <matthias.gabriel@ins.hsr.ch>
2020-12-11 13:58:57 +0100

    cache transformed result

--- pytest_nuts/base_tests/netmiko_cdp_neighbors.py
+++ pytest_nuts/base_tests/netmiko_cdp_neighbors.py
@@ -19,21 +19,26 @@ class CdpNeighborsContext(NutsContext):
         return {"command_string": "show cdp neighbors detail", "use_textfsm": True}
 
     def nornir_filter(self):
-        breakpoint()
-        hosts = {entry["host"] for entry in self.nuts_parameters}
+        hosts = {entry["host"] for entry in self.nuts_parameters["test_data"]}
         return F(name__any=hosts)
 
     def _transform_host_results(self, host_results: MultiResult) -> dict:
         return {neighbor["destination_host"]: neighbor for neighbor in host_results[0].result}
 
+    @property
     def transformed_result(self):
+        if not self._transformed_result:
+            self._transformed_result = self.transform_result()
+        return self._transformed_result
+
+    def transform_result(self):
         general_result = self.general_result()
-        return {host: nuts_result_wrapper(result, self._transform_host_results) for host, result in general_result.items()}
+        return {host: nuts_result_wrapper(result, self._transform_host_results) for host, result in
+                general_result.items()}
 
     def single_result(self, host):
-        transformed_result = self.transformed_result()
-        assert host in transformed_result, f"Host {host} not found in aggregated result."
-        return transformed_result[host]
+        assert host in self.transformed_result, f"Host {host} not found in aggregated result."
+        return self.transformed_result[host]
 
 
 CONTEXT = CdpNeighborsContext

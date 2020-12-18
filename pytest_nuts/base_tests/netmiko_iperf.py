3ec6f65004b65029f8f730f603bad57184a8dffc
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-12-18 10:33:59 +0100

    implement iperf test

--- pytest_nuts/base_tests/netmiko_iperf.py
+++ pytest_nuts/base_tests/netmiko_iperf.py
@@ -10,7 +10,6 @@ from pytest_nuts.helpers.result import nuts_result_wrapper, NutsResult
 
 
 class TestNetmikoIperf:
-
     @pytest.fixture(scope="class")
     def nuts_setup_teardown(self, nuts_parameters, initialized_nornir):
         selected_destinations = initialized_nornir.filter(F(hostname__any=destinations(nuts_parameters["test_data"])))
@@ -42,23 +41,23 @@ class TestNetmikoIperf:
         overall_results = selected_hosts.run(task=nuts_task, **nuts_arguments)
         return overall_results
 
-
     @pytest.fixture(scope="class")
     def hosts(self, nuts_parameters):
         return {entry["host"] for entry in nuts_parameters["test_data"]}
 
     @pytest.fixture(scope="class")
-    def transformed_result(self, general_result, nuts_parameters):
-        return transform_result(general_result, nuts_parameters["test_data"])
+    def transformed_result(self, general_result):
+        return transform_result(general_result)
 
     @pytest.fixture
-    def single_result(selfself, transformed_result, host):
+    def single_result(self, transformed_result, host, destination):
         assert host in transformed_result, f"Host {host} not found in aggregated result."
-        return transformed_result[host]
+        assert destination in transformed_result[host], f"Destination {destination} not found in result."
+        return transformed_result[host][destination]
 
     @pytest.mark.nuts("host,destination,min_expected")
     def test_iperf(self, single_result, host, destination, min_expected):
-        assert single_result
+        assert single_result.result > min_expected
 
 
 def _destinations_per_host(test_data):
@@ -76,31 +75,28 @@ def netmiko_run_iperf(task: Task, destinations_per_host, **kwargs) -> Result:
     dests = destinations_per_host(task.host.name)
     for destination in dests:
         result = task.run(task=_client_iperf, dest=destination, **kwargs)
-        # result[0].destination = destination
     return Result(host=task.host, result=f"iperf executed for {task.host}")
 
 
-def transform_result(general_result, test_data) -> Dict[str, Dict[str, NutsResult]]:
-    return {host: _parse_iperf_result(task_results, test_data) for host, task_results in general_result.items()}
+def transform_result(general_result) -> Dict[str, Dict[str, NutsResult]]:
+    return {host: _parse_iperf_result(task_results) for host, task_results in general_result.items()}
 
 
-def _parse_iperf_result(str, task_results: MultiResult, test_data: List[dict]):
-    min_bps_per_destination = {
-        entry["destination"]: entry["min_expected"] for entry in test_data
-    }
-    results_per_hosts = {}
-    for iperf_task in task_results[1]:
-        iperf_result = json.loads(iperf_task.result)
-        results_per_hosts[iperf_result["start"]["connected"]["remoste_host"]] = iperf_result["end"]["sum_received"]["bits_per_second"]
+def _parse_iperf_result(task_results: MultiResult):
+    results_per_host = {}
+    for iperf_task in task_results[1:]:
+        results_per_host[_extract_dest(iperf_task)] = nuts_result_wrapper(iperf_task, _extract_bps)
+    return results_per_host
 
-    # TODO
-    return {
-        json.loads(iperf_task.result)
 
-        for iperf_task in task_results[1:]
-    }
-        # nuts_result_wrapper(result, _transform_single_result)
-        # response['end']['sum_received']['bits_per_second']
+def _extract_bps(iperf_task):
+    iperf_result = json.loads(iperf_task[1].result)
+    return int(iperf_result["end"]["sum_received"]["bits_per_second"])
+
+
+def _extract_dest(iperf_task):
+    iperf_result = json.loads(iperf_task[1].result)
+    return iperf_result["start"]["connected"][0]["remote_host"]
 
 
 def destinations(test_data):

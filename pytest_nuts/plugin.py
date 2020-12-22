3bb0366804af5a2edfe7f31c21df8cb40074ab12
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-12-22 16:35:22 +0100

    add type annotation to plugin.py

--- pytest_nuts/plugin.py
+++ pytest_nuts/plugin.py
@@ -1,33 +1,43 @@
+from typing import Dict, Callable, Any, Iterable, Union
+
 import pytest
+from _pytest.main import Session
 from _pytest.python import Metafunc
+from _pytest import nodes
 from nornir import InitNornir
+from nornir.core import Nornir
+from nornir.core.filter import F
+from nornir.core.task import AggregatedResult
+from py.path import LocalPath
 
 from pytest_nuts.helpers.result import NutsResult
 from pytest_nuts.yaml2test import NutsYamlFile, get_parametrize_data
 
 
 @pytest.fixture(scope="session")
-def nornir_config_file():
+def nornir_config_file() -> str:
     return "nr-config.yaml"
 
 
 @pytest.fixture(scope="class")
-def nuts_arguments():
+def nuts_arguments() -> Dict:
     return {}
 
 
 @pytest.fixture(scope="class")
-def nornir_filter():
+def nornir_filter() -> None:
     return None
 
 
 @pytest.fixture(scope="session")
-def initialized_nornir(nornir_config_file):
+def initialized_nornir(nornir_config_file: str) -> InitNornir:
     return InitNornir(config_file=nornir_config_file, logging=False)
 
 
 @pytest.fixture(scope="class")
-def general_result(initialized_nornir, nuts_task, nuts_arguments, nornir_filter):
+def general_result(
+    initialized_nornir: Nornir, nuts_task: Callable, nuts_arguments: Dict, nornir_filter: F
+) -> AggregatedResult:
     if nornir_filter:
         selected_hosts = initialized_nornir.filter(nornir_filter)
     else:
@@ -50,11 +60,11 @@ def check_nuts_result(single_result: NutsResult) -> None:
     assert not single_result.failed, "Information gathering failed"
 
 
-def pytest_configure(config):
+def pytest_configure(config) -> None:
     config.addinivalue_line("markers", "nuts: marks the test for nuts parametrization")
 
 
-def pytest_generate_tests(metafunc: Metafunc):
+def pytest_generate_tests(metafunc: Metafunc) -> None:
     """
     Checks if the the nuts pytest parametrization scheme exists (@pytest.mark.nuts)
     to generate tests based on that information. The placeholder later holds data retrieved
@@ -68,6 +78,6 @@ def pytest_generate_tests(metafunc: Metafunc):
 
 
 # https://docs.pytest.org/en/latest/example/nonpython.html#yaml-plugin
-def pytest_collect_file(parent, path):
+def pytest_collect_file(parent: Session, path: LocalPath) -> Iterable[Union[nodes.Item, nodes.Collector]]:
     if path.ext == ".yaml" and path.basename.startswith("test"):
         return NutsYamlFile.from_parent(parent, fspath=path)

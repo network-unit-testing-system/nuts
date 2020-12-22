fe19d6f64203d1428794ad171ee4fbfe1e8bd1c7
Meline Sieber <meline.sieber@ins.hsr.ch>
2020-12-22 17:34:32 +0100

    add typing to host-filer

--- pytest_nuts/host_filter.py
+++ pytest_nuts/host_filter.py
@@ -1,7 +1,9 @@
+from typing import Set
+
 import pytest
 from nornir.core.filter import F
 
 
 @pytest.fixture(scope="class")
-def nornir_filter(hosts) -> F:
+def nornir_filter(hosts: Set) -> F:
     return F(name__any=hosts)

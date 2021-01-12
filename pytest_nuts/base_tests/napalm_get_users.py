100c74ba32c3708aeadbc9bb8f779aa5222779d5
Matthias Gabriel <matthias.gabriel@ost.ch>
2021-01-12 09:15:58 +0100

    refactor users to nuts context

--- pytest_nuts/base_tests/napalm_get_users.py
+++ pytest_nuts/base_tests/napalm_get_users.py
@@ -1,43 +1,35 @@
-from collections import defaultdict
-from typing import Dict
+from typing import Dict, Callable
 
 import pytest
 from nornir.core.filter import F
-from nornir.core.task import MultiResult
+from nornir.core.task import MultiResult, AggregatedResult
 from nornir_napalm.plugins.tasks import napalm_get
 
+from pytest_nuts.context import NornirNutsContext
 from pytest_nuts.helpers.result import nuts_result_wrapper, NutsResult
 
 
-@pytest.fixture(scope="class")
-def nuts_task():
-    return napalm_get
+class UsersContext(NornirNutsContext):
+    def nuts_task(self) -> Callable:
+        return napalm_get
+
+    def nuts_arguments(self) -> dict:
+        return {"getters": ["users"]}
+
+    def nornir_filter(self) -> F:
+        hosts = {entry["host"] for entry in self.nuts_parameters["test_data"]}
+        return F(name__any=hosts)
+
+    def transform_result(self, general_result: AggregatedResult) -> Dict[str, NutsResult]:
+        return {
+            host: nuts_result_wrapper(result, self._transform_host_results) for host, result in general_result.items()
+        }
+
+    def _transform_host_results(self, single_result: MultiResult) -> dict:
+        return single_result[0].result["users"]
 
 
-@pytest.fixture(scope="class")
-def nuts_arguments():
-    return {"getters": ["users"]}
-
-
-@pytest.fixture(scope="class")
-def nornir_filter(hosts):
-    return F(name__any=hosts)
-
-
-@pytest.fixture(scope="class")
-def hosts(nuts_parameters):
-    return {entry["host"] for entry in nuts_parameters["test_data"]}
-
-
-@pytest.fixture(scope="class")
-def transformed_result(general_result):
-    return transform_result(general_result)
-
-
-@pytest.fixture
-def single_result(transformed_result, host):
-    assert host in transformed_result, f"Host {host} not found in aggregated result."
-    return transformed_result[host]
+CONTEXT = UsersContext
 
 
 @pytest.mark.usefixtures("check_nuts_result")
@@ -59,11 +51,3 @@ class TestNapalmOnlyDefinedUsersExist:
     @pytest.mark.nuts("host,usernames")
     def test_no_rogue_users(self, single_result, usernames):
         assert list(single_result.result.keys()) == usernames
-
-
-def transform_result(general_result) -> Dict[str, NutsResult]:
-    return {host: nuts_result_wrapper(result, _transform_single_result) for host, result in general_result.items()}
-
-
-def _transform_single_result(single_result: MultiResult) -> dict:
-    return single_result[0].result["users"]
--- tests/base_tests/test_napalm_get_users.py
+++ tests/base_tests/test_napalm_get_users.py
@@ -1,7 +1,7 @@
 import pytest
 from nornir.core.task import AggregatedResult, MultiResult, Result
 
-from pytest_nuts.base_tests.napalm_get_users import transform_result
+from pytest_nuts.base_tests.napalm_get_users import CONTEXT
 
 test_data = [
     {"host": "R1", "username": "arya", "password": "stark", "level": 11},
@@ -18,6 +18,11 @@ result_data = [
     },
     {"users": {"jon": {"level": 5, "password": "snow", "sshkeys": []}}},
 ]
+
+
+@pytest.fixture
+def nuts_ctx():
+    return CONTEXT(None)
 
 
 @pytest.fixture
@@ -46,26 +51,26 @@ def _tupelize_dict(dict_data):
 
 class TestTransformResult:
     @pytest.mark.parametrize("host", ["R1", "R2", "R3"])
-    def test_contains_host_at_toplevel(self, general_result, host):
-        transformed_result = transform_result(general_result)
+    def test_contains_host_at_toplevel(self, nuts_ctx, general_result, host):
+        transformed_result = nuts_ctx.transform_result(general_result)
         assert host in transformed_result
 
     @pytest.mark.parametrize("host,username", [("R1", "arya"), ("R1", "bran")])
-    def test_contains_multiple_usernames_per_host(self, general_result, host, username):
-        transformed_result = transform_result(general_result)
+    def test_contains_multiple_usernames_per_host(self, nuts_ctx, general_result, host, username):
+        transformed_result = nuts_ctx.transform_result(general_result)
         assert username in transformed_result[host].result
 
     @pytest.mark.parametrize("host,username,password,level", _tupelize_dict(test_data))
-    def test_username_has_corresponding_password(self, general_result, host, username, password, level):
-        transformed_result = transform_result(general_result)
+    def test_username_has_corresponding_password(self, nuts_ctx, general_result, host, username, password, level):
+        transformed_result = nuts_ctx.transform_result(general_result)
         assert transformed_result[host].result[username]["password"] == password
 
     @pytest.mark.parametrize("host,username,password,level", _tupelize_dict(test_data))
-    def test_username_has_matching_privilegelevel(self, general_result, host, username, password, level):
-        transformed_result = transform_result(general_result)
+    def test_username_has_matching_privilegelevel(self, nuts_ctx, general_result, host, username, password, level):
+        transformed_result = nuts_ctx.transform_result(general_result)
         assert transformed_result[host].result[username]["level"] == level
 
-    def test_marks_as_failed_if_task_failed(self, general_result):
-        transformed_result = transform_result(general_result)
+    def test_marks_as_failed_if_task_failed(self, nuts_ctx, general_result):
+        transformed_result = nuts_ctx.transform_result(general_result)
         assert transformed_result["R3"].failed
         assert transformed_result["R3"].exception is not None

3d80e06fa00727ea7c780f598a0529501d3b2d50
Meline Sieber <meline.sieber@ins.hsr.ch>
2021-02-19 14:54:41 +0100

    update mypy.ini to ignore missing py.typed
    py.typed will be included in a later release: https://github.com/nornir-automation/nornir/pull/636

--- pytest_nuts/plugin.py
+++ pytest_nuts/plugin.py
@@ -5,10 +5,9 @@ from _pytest.main import Session
 from _pytest.nodes import Collector
 from _pytest.python import Metafunc
 from _pytest.fixtures import FixtureRequest
-from _pytest import nodes
 from _pytest.config import Config
-from nornir import InitNornir  # type: ignore
-from nornir.core import Nornir  # type: ignore
+from nornir import InitNornir
+from nornir.core import Nornir
 from py._path.local import LocalPath
 
 from pytest_nuts.context import NutsContext, NornirNutsContext

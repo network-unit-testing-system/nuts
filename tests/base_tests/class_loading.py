1891ea8b513ab347480ae2a4aa2e8a85f1e19bb7
Meline Sieber <meline.sieber@ost.ch>
2021-03-02 10:27:36 +0100

    add integration tests for test_execution field

--- tests/base_tests/class_loading.py
+++ tests/base_tests/class_loading.py
@@ -1,6 +1,9 @@
-class TestClass:
-    """ Needed in integration test in test_class_loading.py"""
+"""
+Classes in this file are needed in integration test in test_class_loading.py
+"""
 
+
+class TestClass:
     def test_always_true(self):
         assert True
 
@@ -9,7 +12,15 @@ class TestClass:
 
 
 class TestFixture:
-    """ Needed in integration test in test_class_loading.py"""
-
-    def test_always_true(self, nuts_ctx):
+    def test_nuts_parameters_correctly_injected(self, nuts_ctx):
         assert nuts_ctx.nuts_parameters["test_data"] == ["test1", "test2"]
+
+
+class TestTestExecutionParamsEmpty:
+    def test_nuts_test_execution(selfs, nuts_ctx):
+        assert nuts_ctx.nuts_arguments() == {}
+
+
+class TestTestExecutionParamsExist:
+    def test_nuts_test_execution(selfs, nuts_ctx):
+        assert nuts_ctx.nuts_arguments() == {"count": 42, "ref": 23}

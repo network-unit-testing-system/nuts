66bd5eb5d0fb9e11659a803a6619e4d6210140ce
Meline Sieber <meline.sieber@ost.ch>
2021-03-15 14:41:52 +0100

    use more precise yaml-filenames in class loading tests

--- tests/test_class_loading.py
+++ tests/test_class_loading.py
@@ -21,7 +21,7 @@ def test_load_class_and_execute_tests(testdir):
 
 def test_load_class_multiple_times(testdir):
     arguments = {
-        "test_class_loading": """
+        "test_load_class_repeatedly": """
             ---
             - test_module: tests.base_tests.class_loading
               test_class: TestClass
@@ -39,7 +39,7 @@ def test_load_class_multiple_times(testdir):
 
 def test_injects_arguments_as_fixture(testdir):
     arguments = {
-        "test_class_loading": """
+        "test_args_as_fixture": """
             ---
             - test_module: tests.base_tests.class_loading
               test_class: TestFixture
@@ -56,7 +56,7 @@ def test_load_class_from_index(testdir):
     with patch("pytest_nuts.yaml_to_test.ModuleIndex") as module_index:
         module_index.return_value = ModuleIndex({"TestFixture": "tests.base_tests.class_loading"})
         arguments = {
-            "test_class_loading": """
+            "test_index_loading": """
                 ---
                 - test_class: TestFixture
                   test_data: ['test1', 'test2']
@@ -70,7 +70,7 @@ def test_load_class_from_index(testdir):
 
 def test_class_with_empty_test_execution_field(testdir):
     arguments = {
-        "test_class_loading": """
+        "test_empty_execution_field": """
             ---
             - test_module: tests.base_tests.class_loading
               test_class: TestTestExecutionParamsEmpty
@@ -86,7 +86,7 @@ def test_class_with_empty_test_execution_field(testdir):
 
 def test_class_with_test_execution_field(testdir):
     arguments = {
-        "test_class_loading": """
+        "test_execution_field": """
             ---
             - test_module: tests.base_tests.class_loading
               test_class: TestTestExecutionParamsExist

f67c252038e5e05b470e68547d5674d96fc8e144
Meline Sieber <meline.sieber@ost.ch>
2021-03-16 10:39:02 +0100

    blackify

--- tests/base_tests/simple_nuts_annotation.py
+++ tests/base_tests/simple_nuts_annotation.py
@@ -1,39 +1,34 @@
+"""
+Classes in this file are needed in integration test in test_nuts_annotation.py
+"""
 import pytest
 
 
 class TestKeyValue:
-    """Needed in integration test in test_nuts_annotation.py"""
-
     @pytest.mark.nuts("key,value")
     def test_key_value(self, key, value):
         assert key == value
 
 
 class TestSpacedKeyValue:
-    """Needed in integration test in test_nuts_annotation.py"""
-
     @pytest.mark.nuts(" key , value")
     def test_key_value(self, key, value):
         assert key == value
 
 
 class TestOptionalAttribute:
-    """Needed in integration test in test_nuts_annotation.py"""
-
     @pytest.mark.nuts("key,value", "key")
     def test_key_value(self, key, value):
         assert key == value
 
 
 class TestOptionalAttributes:
-    """Needed in integration test in test_nuts_annotation.py"""
-
     @pytest.mark.nuts("key,value", "key,value")
     def test_key_value(self, key, value):
         assert key == value
 
 
 class TestSingleValue:
-    @pytest.mark.nuts("value", "placeholder")
+    @pytest.mark.nuts("value")
     def test_key_value(self, value):
         assert value == "test"

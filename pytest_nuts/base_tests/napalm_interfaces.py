7253eeab3aaeed39c95041e42c5e49e28d6a394e
Meline Sieber <meline.sieber@ost.ch>
2021-03-23 15:33:49 +0100

    setup context class

--- pytest_nuts/base_tests/napalm_interfaces.py
+++ pytest_nuts/base_tests/napalm_interfaces.py
@@ -1,25 +1,37 @@
+"""Query interfaces and their information of a device."""
+from typing import Dict, Callable, List
+
 import pytest
+from nornir.core.filter import F
+from nornir.core.task import MultiResult
+from nornir_napalm.plugins.tasks import napalm_get
+
+from pytest_nuts.context import NornirNutsContext
+from pytest_nuts.helpers.converters import InterfaceNameConverter
+from pytest_nuts.helpers.result import nuts_result_wrapper, NutsResult
 
 from nornir_napalm.plugins.tasks import napalm_get
 
 # TODO not implemented
 
+class InterfacesContext(NornirNutsContext):
+    def nuts_task(self) -> Callable:
+        return napalm_get
 
-@pytest.fixture(scope="class")
-def nuts_task():
-    return napalm_get
+    def nuts_arguments(self) -> Dict[str, List]:
+        return {"getters": ["interfaces"]}
 
+    def nornir_filter(self) -> F:
+        hosts = {entry["host"] for entry in self.nuts_parameters["test_data"]}
+        return F(name__any=hosts)
 
-@pytest.fixture(scope="class")
-def nuts_arguments():
-    return {"getters": ["interfaces_ip"]}
+    def transform_result(self, general_result) -> Dict[str, NutsResult]:
+        return {
+            host: nuts_result_wrapper(result, self._transform_host_results) for host, result in general_result.items()
+        }
 
+CONTEXT = InterfacesContext
 
-@pytest.fixture(scope="class")
-def hosts(hosts_interface_list):
-    return {entry["source"] for entry in hosts_interface_list}
-
-
-@pytest.fixture(scope="class")
-def transformed_result(general_result):
-    return {key: value[0].result for key, value in general_result.items()}
+@pytest.mark.usefixtures("check_nuts_result")
+class TestInterfaces:
+    pass

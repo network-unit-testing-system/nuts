ba964969e99cf19498e4b0b280d13b5e8e323dad
Meline Sieber <meline.sieber@ost.ch>
2021-04-12 16:56:06 +0200

    refactor iperf test to use new helper function, add test that checks for failed iperf task

--- tests/base_tests/test_netmiko_iperf.py
+++ tests/base_tests/test_netmiko_iperf.py
@@ -1,56 +1,52 @@
 import pytest
+from nornir.core.exceptions import ConnectionException
 
 from nornir.core.task import AggregatedResult, MultiResult, Result
 from pytest_nuts.base_tests.netmiko_iperf import CONTEXT
+from tests.base_tests.conftest import TIMEOUT_MESSAGE
+from tests.helpers.shared import create_result, create_multi_result
 
-test_data = [
-    {"host": "L1", "destination": "10.0.0.2", "min_expected": 10000000},
-    {"host": "L1", "destination": "10.0.0.3", "min_expected": 10000000},
-    {"host": "L2", "destination": "10.0.0.1", "min_expected": 10000000},
+test_data_and_nornir_results = [
+    {
+        "test_data": {"host": "L1", "destination": "10.0.0.2", "min_expected": 10000000},
+        "nornir_result": '{"start":{"connected":[{"remote_host":"10.0.0.2"}]},"end":{"sum_received":{"bits_per_second":3.298164e09}}}',
+    },
+    {
+        "test_data": {"host": "L1", "destination": "10.0.0.3", "min_expected": 10000000},
+        "nornir_result": '{"start":{"connected":[{"remote_host":"10.0.0.3"}]},"end":{"sum_received":{"bits_per_second":3.298164e09}}}',
+    },
+    {
+        "test_data": {"host": "L2", "destination": "10.0.0.1", "min_expected": 10000000},
+        "nornir_result": '{"start":{"connected":[{"remote_host":"10.0.0.1"}]},"end":{"sum_received":{"bits_per_second": 5000}}}',
+    },
+    {
+        "test_data": {"host": "L2", "destination": "10.0.0.220", "min_expected": 10000000},
+        "nornir_result": '{	"start":{"connected":[],"version":"iperf 3.1.3","system_info":"Linux"},"intervals":[],"end":{},"error":"error - unable to connect to server: No route to host"}',
+    },
 ]
 
-result_data = [
-    '{"start":{"connected":[{"remote_host":"10.0.0.2"}]},"end":{"sum_received":{"bits_per_second":3.298164e09}}}',
-    '{"start":{"connected":[{"remote_host":"10.0.0.3"}]},"end":{"sum_received":{"bits_per_second":3.298164e09}}}',
-    '{"start":{"connected":[{"remote_host":"10.0.0.1"}]},"end":{"sum_received":{"bits_per_second":0}}}',
-]
+reachable_hosts = [tuple(entry["test_data"].values()) for entry in test_data_and_nornir_results[:-1]]
 
 
 @pytest.fixture
 def general_result():
-    ag_result = AggregatedResult("netmiko_iperf")
-
-    overall_mr_l1 = MultiResult("netmiko_run_iperf")
-    overall_mr_l1.append(Result(host=None, name="netmiko_run_iperf", result="iperf executed"))
-
-    mr_l1_1 = MultiResult(name="_client_iperf")
-    mr_l1_1.append(Result(host=None, name="_client_iperf", result="iperf executed"))
-    l1_result1 = Result(host=None, name="netmiko_send_command")
-    l1_result1.result = result_data[0]
-    mr_l1_1.append(l1_result1)
-    overall_mr_l1.append(mr_l1_1)
-
-    mr_l1_2 = MultiResult(name="_client_iperf")
-    mr_l1_2.append(Result(host=None, name="_client_iperf", result="iperf executed"))
-    l1_result2 = Result(host=None, name="netmiko_send_command")
-    l1_result2.result = result_data[1]
-    mr_l1_2.append(l1_result2)
-    overall_mr_l1.append(mr_l1_2)
-
-    ag_result["L1"] = overall_mr_l1
-
-    overall_mr_l2 = MultiResult("netmiko_run_iperf")
-    overall_mr_l2.append(Result(host=None, name="netmiko_run_iperf", result="iperf executed"))
-    mr_l2_1 = MultiResult(name="_client_iperf")
-    mr_l2_1.append(Result(host=None, name="_client_iperf", result="iperf executed"))
-    l2_result1 = Result(host=None, name="netmiko_send_command")
-    l2_result1.result = result_data[2]
-    mr_l2_1.append(l2_result1)
-    overall_mr_l2.append(mr_l2_1)
-
-    ag_result["L2"] = overall_mr_l2
-
-    return ag_result
+    task_name = "netmiko_run_iperf"
+    results = [
+        create_result(
+            data["nornir_result"],
+            task_name,
+            host=data["test_data"]["host"],
+            destination=data["test_data"]["destination"],
+        )
+        for data in test_data_and_nornir_results
+    ]
+    confirmation_result = create_result(result_content="iperf executed for host", task_name=task_name)
+    general_result = AggregatedResult(task_name)
+    general_result["L1"] = create_multi_result(
+        results=[confirmation_result, results[0], results[1]], task_name=task_name
+    )
+    general_result["L2"] = create_multi_result(results=[confirmation_result, results[2], results[3]], task_name=task_name)
+    return general_result
 
 
 pytestmark = [pytest.mark.nuts_test_ctx(CONTEXT())]
@@ -61,23 +57,19 @@ class TestTransformResult:
     def test_contains_host_at_toplevel(self, transformed_result, host):
         assert host in transformed_result
 
-    @pytest.mark.parametrize(
-        "host, destination",
-        [
-            (test_data[0]["host"], test_data[0]["destination"]),
-            (test_data[1]["host"], test_data[1]["destination"]),
-            (test_data[2]["host"], test_data[2]["destination"]),
-        ],
-    )
-    def test_contains_iperf_dest(self, transformed_result, host, destination):
+    @pytest.mark.parametrize("host, destination, min_expected", reachable_hosts)
+    def test_contains_iperf_dest(self, transformed_result, host, destination, min_expected):
         assert destination in transformed_result[host]
 
-    @pytest.mark.parametrize(
-        "host, destination, min_expected", [tuple(test_data[0].values()), tuple(test_data[1].values())]
-    )
+    @pytest.mark.parametrize("host, destination, min_expected", reachable_hosts[:1])
     def test_one_host_several_destinations(self, transformed_result, host, destination, min_expected):
-        assert transformed_result[host][destination].result > min_expected
+        assert transformed_result[host][destination].result >= min_expected
 
-    @pytest.mark.parametrize("host, destination, min_expected", [tuple(test_data[2].values())])
-    def test_min_expected_fails(self, transformed_result, host, destination, min_expected):
-        assert transformed_result[host][destination].result != min_expected
+    @pytest.mark.parametrize("host, destination, min_expected", [reachable_hosts[2]])
+    def test_below_min_expected_fails(self, transformed_result, host, destination, min_expected):
+        assert not transformed_result[host][destination].result >= min_expected
+
+    @pytest.mark.parametrize("host, destination, min_expected", [tuple(test_data_and_nornir_results[3]["test_data"].values())])
+    def test_dest_unreachable_failes(self, transformed_result, host, destination, min_expected):
+        assert transformed_result[host][destination].failed
+        assert transformed_result[host][destination].exception is not None

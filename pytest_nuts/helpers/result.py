10996a12a32c3c7f43c37dd5d485578012e9ee9d
Meline Sieber <meline.sieber@ost.ch>
2021-04-12 15:25:20 +0200

    add docstring to helper functions

--- pytest_nuts/helpers/result.py
+++ pytest_nuts/helpers/result.py
@@ -31,22 +31,52 @@ T = TypeVar("T", Result, MultiResult, AggregatedResult)
 def map_host_to_nutsresults(
     general_result: AggregatedResult, single_transform: Callable[[MultiResult], Any]
 ) -> Dict[str, NutsResult]:
+    """
+    Maps a host's name to its corresponding result, which in turn is
+    wrapped into a NutsResult.
+
+    Used when a nornir tasks queries properties of a host.
+
+    :param general_result: The raw result as provided by nornir's executed task
+    :param single_transform: function to be applied to a nornir MultiResult
+    :return: Host mapped to a NutsResult
+    """
     return {host: nuts_result_wrapper(multiresult, single_transform) for host, multiresult in general_result.items()}
 
 
 def map_host_to_dest_to_nutsresult(
     general_result: AggregatedResult, single_transform: Callable[[Result], Any]
 ) -> Dict[str, Dict[str, NutsResult]]:
+    """
+    Maps a host's name to its corresponding destination and calls a helper function to further map
+    that destination to a NutsResult.
+
+    Used when a nornir task queries a host-destination pair.
+
+    :param general_result: The raw result as provided by nornir's executed task
+    :param single_transform: function to be applied to a nornir Result
+    :return: The host mapped to its corresponding destination mapped to its NutsResult
+    """
     return {
         host: map_dest_to_nutsresult(task_results, single_transform) for host, task_results in general_result.items()
     }
 
 
 def map_dest_to_nutsresult(task_results: MultiResult, single_transform: Callable[[Result], Any]) -> Dict[str, NutsResult]:
-    # the destination is not included in the nornir result if the ping fails
-    # therefore we cannot know which destination was not reachable
-    # so we had to patch the destination onto the result object to know later which
-    # host-destination pair actually failed - but mypy does not know about this
+    """
+    Maps a destination to the result of a nornir task which both belong to a host.
+
+    Note: The nornir Result object does not contain a destination property, which often means that
+    if the specific task fails (e.g. a ping fails), the destination information is missing in the result,
+    and we do not know which destination actually failed.
+    It is therefore necessary to patch the destination onto the Result object to later know which
+    host-destination pair actually failed.
+    As a consequence, typing must ignore the patched attribute.
+
+    :param task_results: The results of the nornir task per host
+    :param single_transform: function to be applied to a single nornir Result
+    :return: The destination mapped to the desired information
+    """
     return {
         single_result.destination: nuts_result_wrapper(single_result, single_transform)  # type: ignore[attr-defined]
         for single_result in task_results[1:]

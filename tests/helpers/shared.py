85fc774bdbdc7f17650c6b894d12b0b8c70be484
Meline Sieber <meline.sieber@ost.ch>
2021-04-12 11:12:59 +0200

    call create_multi_result with a list of results

--- tests/helpers/shared.py
+++ tests/helpers/shared.py
@@ -1,7 +1,7 @@
 """
 Helper module with data used by all tests.
 """
-from typing import Any, Optional
+from typing import Any, Optional, List
 
 from nornir.core.task import MultiResult, Result
 
@@ -14,16 +14,15 @@ class Host():
     def __init__(self, name: str):
         self.name = name
 
-def create_multi_result(
-    result_content: Any, task_name: str, host: Optional[Host]=None, destination: Optional[str]=None, failed: bool = False, exception: Optional[BaseException] = None
+def create_multi_result(results: List[Result], task_name: str
 ) -> MultiResult:
     multi_result = MultiResult(task_name)
-    result = create_result(result_content, task_name, host, destination, failed, exception)
-    multi_result.append(result)
+    for result in results:
+        multi_result.append(result)
     return multi_result
 
 
-def create_result(result_content: Any, task_name: str, host: Optional[Host], destination: Optional[str]=None, failed: bool = False, exception: Optional[BaseException] = None, **kwargs
+def create_result(result_content: Any, task_name: str, host: Optional[Host]=None, destination: Optional[str]=None, failed: bool = False, exception: Optional[BaseException] = None, **kwargs
 ) -> Result:
     result = Result(host=host, name=task_name, destination=destination, **kwargs)
     result.result = result_content

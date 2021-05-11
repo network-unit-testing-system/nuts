fc03b8cb7747b38f4f8630c16def7a234a0d1603
Meline Sieber <meline.sieber@ost.ch>
2021-05-11 08:22:58 +0200

    rename file from shared.py to something more meaningful

--- tests/helpers/selftest_helpers.py
+++ tests/helpers/selftest_helpers.py
@@ -1,12 +1,29 @@
 """
 Helper module with data used by all tests.
 """
-from typing import Any, Optional, List
+from dataclasses import dataclass
+from typing import Any, Optional, List, Dict
 
 from nornir.core.inventory import Host
 from nornir.core.task import MultiResult, Result
 
 YAML_EXTENSION = ".yaml"
+
+@dataclass
+class SelfTestData:
+    """
+    Helper-Class that pairs the raw result that is sent over the wire with the test data (expected values).
+    """
+    nornir_raw_result: Dict[str, Any]
+    test_data: Dict[str, Any]
+
+    def create_nornir_result(self, task_name: str):
+        return create_result(
+            result_content=self.nornir_raw_result,
+            task_name=task_name,
+            host=self.test_data["host"],
+            destination=self.test_data.get("destination", None)
+        )
 
 
 def create_multi_result(results: List[Result], task_name: str) -> MultiResult:
@@ -30,3 +47,5 @@ def create_result(
     result.failed = failed
     result.exception = exception
     return result
+
+

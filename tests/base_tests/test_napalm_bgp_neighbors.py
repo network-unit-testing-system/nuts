59734ea22ed70a0783277d4056ae713b8beaebac
Meline Sieber <meline.sieber@ost.ch>
2021-05-31 11:46:57 +0200

    refactor bgp neighbors self-test

--- tests/base_tests/test_napalm_bgp_neighbors.py
+++ tests/base_tests/test_napalm_bgp_neighbors.py
@@ -4,12 +4,16 @@ from nornir.core.task import AggregatedResult
 from nuts.base_tests.napalm_bgp_neighbors import CONTEXT
 from tests.helpers.selftest_helpers import create_multi_result, SelfTestData
 
+R1_IP = "172.16.255.1"
+R2_IP = "172.16.255.2"
+R3_IP = "172.16.255.3"
+
 nornir_raw_r1 = {
     "bgp_neighbors": {
         "global": {
-            "router_id": "172.16.255.1",
+            "router_id": R1_IP,
             "peers": {
-                "172.16.255.2": {
+                R2_IP: {
                     "local_as": 45001,
                     "remote_as": 45002,
                     "remote_id": "0.0.0.0",
@@ -21,7 +25,7 @@ nornir_raw_r1 = {
                         "ipv4 unicast": {"received_prefixes": -1, "accepted_prefixes": -1, "sent_prefixes": -1}
                     },
                 },
-                "172.16.255.3": {
+                R3_IP: {
                     "local_as": 45001,
                     "remote_as": 45003,
                     "remote_id": "0.0.0.0",
@@ -41,9 +45,9 @@ nornir_raw_r1 = {
 nornir_raw_r2 = {
     "bgp_neighbors": {
         "global": {
-            "router_id": "172.16.255.2",
+            "router_id": R2_IP,
             "peers": {
-                "172.16.255.1": {
+                R1_IP: {
                     "local_as": 45002,
                     "remote_as": 45001,
                     "remote_id": "0.0.0.0",
@@ -65,8 +69,8 @@ bgp_r1_1 = SelfTestData(
     nornir_raw_result=nornir_raw_r1,
     test_data={
         "host": "R1",
-        "local_id": "172.16.255.1",
-        "peer": "172.16.255.2",
+        "local_id": R1_IP,
+        "peer": R2_IP,
         "is_enabled": True,
         "remote_as": 45002,
         "remote_id": "0.0.0.0",
@@ -79,8 +83,8 @@ bgp_r1_2 = SelfTestData(
     nornir_raw_result=nornir_raw_r1,
     test_data={
         "host": "R1",
-        "local_id": "172.16.255.1",
-        "peer": "172.16.255.3",
+        "local_id": R1_IP,
+        "peer": R3_IP,
         "is_enabled": True,
         "remote_as": 45001,
         "remote_id": "0.0.0.0",
@@ -93,8 +97,8 @@ bgp_r2 = SelfTestData(
     nornir_raw_result=nornir_raw_r2,
     test_data={
         "host": "R2",
-        "local_id": "172.16.255.2",
-        "peer": "172.16.255.1",
+        "local_id": R2_IP,
+        "peer": R1_IP,
         "is_enabled": True,
         "remote_as": 45001,
         "remote_id": "0.0.0.0",
@@ -127,19 +131,26 @@ pytestmark = [pytest.mark.nuts_test_ctx(CONTEXT())]
 
 class TestTransformResult:
     def test_contains_hosts_at_toplevel(self, transformed_result):
-        assert all(h in transformed_result for h in ["R1", "R2", "R3"])
+        assert transformed_result.keys() == {"R1", "R2", "R3"}
 
-    def test_contains_peers_at_second_level(self, transformed_result, all_testdata):
-        assert all(entry["peer"] in transformed_result[entry["host"]].result for entry in all_testdata)
+    @pytest.mark.parametrize("host, neighbors", [("R1", {R2_IP, R3_IP}), ("R2", {R1_IP})])
+    def test_contains_peers_at_second_level(self, transformed_result, host, neighbors):
+        assert transformed_result[host].result.keys() == neighbors
 
     def test_contains_information_about_neighbor(self, transformed_result):
         neighbor_details = transformed_result["R1"].result[bgp_r1_1.test_data["peer"]]
-        for key in bgp_r1_1.test_data:
-            if (
-                key == "host" or key == "peer"
-            ):  # skip, because these are keys into the data entries and not part of the test
-                continue
-            assert bgp_r1_1.test_data[key] == neighbor_details[key]
+        expected = {
+            "address_family": {"ipv4 unicast": {"accepted_prefixes": -1, "received_prefixes": -1, "sent_prefixes": -1}},
+            "description": "",
+            "is_enabled": bgp_r1_1.test_data["is_enabled"],
+            "is_up": bgp_r1_1.test_data["is_up"],
+            "local_as": bgp_r1_1.test_data["local_as"],
+            "local_id": R1_IP,
+            "remote_as": bgp_r1_1.test_data["remote_as"],
+            "remote_id": bgp_r1_1.test_data["remote_id"],
+            "uptime": -1,
+        }
+        assert neighbor_details == expected
 
     def test_marks_as_failed_if_task_failed(self, transformed_result):
         assert transformed_result["R3"].failed

ae13cbda4237cecbbfdee9455713f572c4c0e26b
ubaumann <github@m.ubaumann.ch>
2021-07-28 13:24:55 +0200

    Black black

--- nuts/helpers/context.py
+++ nuts/helpers/context.py
@@ -6,7 +6,6 @@ from nuts.context import NutsContext
 from typing import Dict, Any
 
 
-
 def load_context(module: types.ModuleType, params: Dict[str, Any]) -> NutsContext:
     context_class = getattr(module, "CONTEXT", NutsContext)
     ctx = context_class(params)

9d00f7f4432bb047049426bf7b97e0e225d22f01
Severin Grimm <severin.grimm@ost.ch>
2021-11-13 15:58:45 +0100

    add vlan test

--- tests/base_tests/test_napalm_get_vlans.py
+++ tests/base_tests/test_napalm_get_vlans.py
@@ -18,29 +18,30 @@ from tests.utils import create_multi_result, SelfTestData
 # if tag as int work with vlan_name
 
 nornir_raw_result_s1 = {
-    1: {
-        "name": "default",
-        "interfaces": ["GigabitEthernet0/0/1", "GigabitEthernet0/0/2"],
-    },
-    2: {"name": "vlan2", "interfaces": []},
+    "vlans": {
+        "1": {
+            "name": "default",
+            "interfaces": ["GigabitEthernet0/0/1", "GigabitEthernet0/0/2"],
+        },
+        "2": {"name": "vlan2", "interfaces": []},
+    }
+}
+nornir_raw_result_s2 = {
+    "vlans": {
+        "2": {"name": "vlan2", "interfaces": []},
+    }
 }
 
 vlans_s1_1 = SelfTestData(
     name="s1_1",
     nornir_raw_result=nornir_raw_result_s1,
-    test_data={"host": "S1", "vlan_name": "default", "vlan_tag": "1"},
-)
-
-vlans_s1_2 = SelfTestData(
-    name="s1_2",
-    nornir_raw_result=nornir_raw_result_s1,
-    test_data={"host": "S1", "vlan_tag": "2"},
+    test_data={"host": "S1", "vlan_name": "default", "vlan_tag": 1},
 )
 
 vlans_s2 = SelfTestData(
     name="s2",
-    nornir_raw_result={2: {"name": "vlan2", "interfaces": []}},
-    test_data={"host": "S2", "vlan_name": "default", "vlan_tag": "2"},
+    nornir_raw_result=nornir_raw_result_s2,
+    test_data={"host": "S2", "vlan_name": "vlan2", "vlan_tag": 2},
 )
 
 
@@ -49,16 +50,16 @@ def general_result(timeouted_multiresult):
     task_name = "napalm_get_facts"
     result = AggregatedResult(task_name)
     result["S1"] = create_multi_result(
-        [vlans_s1_1.create_nornir_result(), vlans_s1_2.create_nornir_result()],
+        [vlans_s1_1.create_nornir_result()],
         task_name,
     )
     result["S2"] = create_multi_result([vlans_s2.create_nornir_result()], task_name)
-    # result["S3"] = timeouted_multiresult
+    result["S3"] = timeouted_multiresult
     return result
 
 
 @pytest.fixture(
-    params=[vlans_s1_1, vlans_s1_2, vlans_s2],
+    params=[vlans_s1_1, vlans_s2],
     ids=lambda data: data.name,
 )
 def selftestdata(request):
@@ -75,7 +76,7 @@ pytestmark = [pytest.mark.nuts_test_ctx(CONTEXT())]
 
 def test_contains_host_at_toplevel(transformed_result):
     print(transformed_result)
-    assert transformed_result.keys() == {"S1", "S2"}
+    assert transformed_result.keys() == {"S1", "S2", "S3"}
 
 
 @pytest.fixture
@@ -85,29 +86,24 @@ def single_result(transformed_result, testdata):
     return host_result.result
 
 
-# def test_contains_multiple_usernames_per_host(single_result, testdata):
-#     assert testdata["vlans"] in single_result
-
-
-# def test_vlan_tag_has_corresponding_vlan_name(single_result, testdata):
-#     print(single_result)
-#     assert single_result[testdata["vlan_tag"]["name"]] == testdata["vlan_name"]
+def test_vlan_tag_has_corresponding_vlan_name(single_result, testdata):
+    assert single_result[testdata["vlan_tag"]]["name"] == testdata["vlan_name"]
 
 
 # def test_username_has_matching_privilegelevel(single_result, testdata):
 #     assert single_result[testdata["username"]]["level"] == testdata["level"]
 
 
-# def test_marks_as_failed_if_task_failed(transformed_result):
-#     assert transformed_result["R3"].failed
-#     assert transformed_result["R3"].exception is not None
+def test_marks_as_failed_if_task_failed(transformed_result):
+    assert transformed_result["S3"].failed
+    assert transformed_result["S3"].exception is not None
 
 
 # def test_integration(selftestdata, integration_tester):
-#     integration_tester(
-#         selftestdata,
-#         test_class="TestNapalmUsers",
-#         task_module=tasks,
-#         task_name="napalm_get",
-#         test_count=3,
-#     )
+#    integration_tester(
+#        selftestdata,
+#        test_class="TestNapalmVlans",
+#        task_module=tasks,
+#        task_name="napalm_get",
+#        test_count=3,
+#    )

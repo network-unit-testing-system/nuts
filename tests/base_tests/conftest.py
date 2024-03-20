d5738645351896de333a264dde12be489ddc02bf
ubaumann <github@m.ubaumann.ch>
2024-03-20 17:31:00 +0100

    Blackify

--- tests/base_tests/conftest.py
+++ tests/base_tests/conftest.py
@@ -114,7 +114,15 @@ def transformed_result(
 
 @pytest.fixture
 def integration_tester(monkeypatch, pytester, default_nr_init):
-    def _run(selftestdata, *, test_class, task_module, task_name, test_count, test_execution = None):
+    def _run(
+        selftestdata,
+        *,
+        test_class,
+        task_module,
+        task_name,
+        test_count,
+        test_execution=None,
+    ):
         yaml_data = [
             {
                 "test_class": test_class,

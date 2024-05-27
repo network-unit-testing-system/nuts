1ec6f5571c5ab7e53b90ff7819ac99422f91cd31
Urs Baumann <github@m.ubaumann.ch>
2024-05-27 14:30:18 +0000

    Add return type annotation for pztest_nuts_single_result

--- nuts/hooks.py
+++ nuts/hooks.py
@@ -5,5 +5,5 @@ from nuts.helpers.result import NutsResult
 
 def pytest_nuts_single_result(
     request: FixtureRequest, nuts_ctx: NutsContext, result: NutsResult
-):
+) -> None:
     """Called in the single result fixture"""
